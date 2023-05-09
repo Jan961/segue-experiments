87ff66e3cb6ee58486c659deecb7d22f71d91b03
Neil Sutcliffe <neil@neilsutcliffe.com>
2023-05-09 11:50:41 +0100

    Extra plugin for react 17 'react import' errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard'
+    'standard',
+    'plugin:react/jsx-runtime'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
