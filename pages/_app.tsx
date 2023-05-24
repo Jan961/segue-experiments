a354322ab46205e8a2c16ca936940beac7522990
Neil Sutcliffe <neil@neilsutcliffe.com>
2023-05-24 15:47:45 +0100

    Disabled login till we are at a stage where it can do something. Kept code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import type { AppProps } from 'next/app'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import {useContext, useEffect, useState} from "react";
-import { userService } from '../services/user.service';
-import {useRouter} from "next/router";
+// import { useEffect, useState } from 'react'
+// import { userService } from '../services/user.service'
+// import { useRouter } from 'next/router'
 import { RecoilRoot } from 'recoil'
 library.add(fab, fas)
 
@@ -19,70 +19,72 @@ type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout
 }
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+// Login disabled due to lack of support. DO NOT PUT PUBLIC
+export default function MyApp ({ Component, pageProps }: AppPropsWithLayout) {
+  /*
+  const router = useRouter()
+  const [user, setUser] = useState(null)
+  const [authorized, setAuthorized] = useState(false)
+  const [setTour, setSetTour] = useState(0)
 
-    const router = useRouter();
-    const [user, setUser] = useState(null);
-    const [authorized, setAuthorized] = useState(false);
-    const [setTour, setSetTour] = useState(0);
+  useEffect(() => {
+    // on initial load - run auth check
+    authCheck(router.asPath)
 
-    useEffect(() => {
-        // on initial load - run auth check
-        authCheck(router.asPath);
+    // on route change start - hide page content by setting authorized to false
+    const hideContent = () => setAuthorized(false)
+    router.events.on('routeChangeStart', hideContent)
 
-        // on route change start - hide page content by setting authorized to false
-        const hideContent = () => setAuthorized(false);
-        router.events.on('routeChangeStart', hideContent);
+    // on route change complete - run auth check
+    router.events.on('routeChangeComplete', authCheck)
 
-        // on route change complete - run auth check
-        router.events.on('routeChangeComplete', authCheck)
-
-        // unsubscribe from events in useEffect return function
-        return () => {
-            router.events.off('routeChangeStart', hideContent);
-            router.events.off('routeChangeComplete', authCheck);
-        }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    function authCheck(url) {
-        // redirect to login page if accessing a private page and not logged in
-        //const appState = useContext(AppState);
-        setUser(userService.userValue);
-        const userDetails = userService.userValue
-        const publicPaths = ['/login', '/register', '/forgotten-password', '/register/[registrationInviteCode]', '/privacy', '/terms', '/account-actions/[userId]/activate/[hash]' , '/account-actions/[userId]/reset/[hash]'];
-        const path = url.split('?')[0];
-        if (!userService.userValue && !publicPaths.includes(path)) {
-            setAuthorized(false);
-            router.push({
-                pathname: '/login',
-                query: { returnUrl: router.asPath }
-            });
-        } else {
-            setAuthorized(true);
-            //Top Level permission Permissions
-            // Segue admin Users Allows to global data
-            if(userDetails !== null) {
-                if (router.pathname.startsWith("/administration") && !userDetails.segueAdmin) {
-                    router.push({
-                        pathname: '/unauthorised',
-                    });
-                }
-                // if User is logged in but has no access to top level Account Permission
-
-                if (router.pathname.startsWith("/accounts") && !userDetails.accountAdmin || !userDetails.accountOwner) {
-                    router.push({
-                        pathname: '/unauthorised',
-                    });
-                }
-            }
-            setAuthorized(true);
-        }
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off('routeChangeStart', hideContent)
+      router.events.off('routeChangeComplete', authCheck)
     }
 
-    // Use the layout defined at the page level, if available
-    const getLayout = Component.getLayout ?? ((page) => page)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-    return getLayout(<RecoilRoot><Component {...pageProps} /></RecoilRoot>)
+  function authCheck (url) {
+    // redirect to login page if accessing a private page and not logged in
+    // const appState = useContext(AppState);
+    setUser(userService.userValue)
+    const userDetails = userService.userValue
+    const publicPaths = ['/login', '/register', '/forgotten-password', '/register/[registrationInviteCode]', '/privacy', '/terms', '/account-actions/[userId]/activate/[hash]', '/account-actions/[userId]/reset/[hash]']
+    const path = url.split('?')[0]
+    if (!userService.userValue && !publicPaths.includes(path)) {
+      setAuthorized(false)
+      router.push({
+        pathname: '/login',
+        query: { returnUrl: router.asPath }
+      })
+    } else {
+      setAuthorized(true)
+      // Top Level permission Permissions
+      // Segue admin Users Allows to global data
+      if (userDetails !== null) {
+        if (router.pathname.startsWith('/administration') && !userDetails.segueAdmin) {
+          router.push({
+            pathname: '/unauthorised'
+          })
+        }
+        // if User is logged in but has no access to top level Account Permission
+
+        if (router.pathname.startsWith('/accounts') && !userDetails.accountAdmin || !userDetails.accountOwner) {
+          router.push({
+            pathname: '/unauthorised'
+          })
+        }
+      }
+      setAuthorized(true)
+    }
+  }
+  */
+
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<RecoilRoot><Component {...pageProps} /></RecoilRoot>)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,39 @@
-import Layout from '../components/Layout'
-import UserMessage from '../components/dashboard/userMessage'
-import Switchboard from '../components/dashboard/switchboard'
-import { useEffect, useState } from 'react'
-import { LoadingPage } from 'components/global/LoadingPage'
+import Layout from 'components/Layout'
+import Switchboard from 'components/dashboard/switchboard'
+// import { useEffect, useState } from 'react'
+// import { LoadingPage } from 'components/global/LoadingPage'
 
-export default function Index() {
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(false)
+const accountId = 0
 
-    let accoutnID = 0
+// Login functionality disabled
+export default function Index () {
+/*
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
 
-    useEffect(() => {
-        setLoading(true)
+    const userid = sessionStorage.getItem('UserId')
+    const accountId = sessionStorage.getItem('accountId')
 
-        let userid = sessionStorage.getItem("UserId")
-        let accountId = sessionStorage.getItem("accountId")
+    fetch('/api/account/read/' + accountId)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
 
+  if (isLoading) return <LoadingPage />
+  if (!data) return <p>No profile data</p>
+  */
 
-        fetch('/api/account/read/' + accountId)
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
-
-    if (isLoading) return <LoadingPage />
-    if (!data) return <p>No profile data</p>
-
-    return (
-        <div >
-            <Layout title="Dashboard | Segue" >
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mt-5 text-center">
-                    <span className="block xl:inline">Home</span>
-
-                </h1>
-
-                <UserMessage></UserMessage>
-                <Switchboard key={"sw"} data={accoutnID}  ></Switchboard>
-            </Layout>
-        </div>
-    )
+  return (
+    <Layout title="Dashboard | Segue" >
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mt-5 text-center">
+        <span className="block xl:inline">Home</span>
+      </h1>
+      <Switchboard key={'sw'} data={accountId} ></Switchboard>
+    </Layout>
+  )
 }
