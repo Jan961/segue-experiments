5e7a73b790c920b903e1daf732c472f4baab94e5
Neil Sutcliffe <neil@neilsutcliffe.com>
2023-06-06 15:44:47 +0100

    Fix build (unused imports)

--- components/bookings/panel/PerformancePanel.tsx
+++ components/bookings/panel/PerformancePanel.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios'
+import { FormInputButton } from 'components/global/forms/FormInputButton'
 import { FormInputDate } from 'components/global/forms/FormInputDate'
 import { FormInputTime } from 'components/global/forms/FormInputTime'
 import { PerformanceDTO } from 'interfaces'
@@ -13,7 +15,7 @@ export const PerformancePanel = ({ performanceId }: PerformancePanelProps) => {
   const performanceDict = useRecoilValue(performanceDictSelector)
   const perf: PerformanceDTO = performanceDict[performanceId]
   const [inputs, setInputs] = React.useState<PerformanceDTO>(perf)
-  const [status, setStatus] = React.useState({ submitting: false, changed: false })
+  const [{ submitting, changed }, setStatus] = React.useState({ submitting: false, changed: false })
 
   const datePart = inputs.Date ? inputs.Date.split('T')[0] : ''
   const timePart = inputs.Date ? inputs.Date.split('T')[1] : ''
@@ -21,23 +23,78 @@ export const PerformancePanel = ({ performanceId }: PerformancePanelProps) => {
   const handleOnChange = (e: any) => {
     const { id, value } = e.target
 
-    setInputs((prev) => ({
-      ...prev,
-      [id]: value
-    }))
+    if (id === 'datePart') {
+      const newDatePart = value.split('T')[0] ? value.split('T')[0] : ''
+      setInputs((prev) => ({
+        ...prev,
+        Date: newDatePart + 'T' + timePart
+      }))
+    } else if (id === 'timePart') {
+      setInputs((prev) => ({
+        ...prev,
+        Date: datePart + 'T' + value
+      }))
+    }
     setStatus({ submitting: false, changed: true })
   }
 
+  const saveDetails = async () => {
+    const response = await axios({
+      method: 'POST',
+      url: '/api/bookings/update/',
+      data: inputs
+    })
+
+    // const updated = response.data
+    // updatePerformance(updated)
+  }
+
+  const save = async (e) => {
+    e.preventDefault()
+    setStatus({ submitting: true, changed: true })
+    try {
+      await saveDetails()
+      setStatus({ submitting: false, changed: false })
+    } catch {
+      alert('An error occured while submitting')
+      setStatus({ submitting: false, changed: true })
+    }
+  }
+
+  const initiateDelete = () => {
+    alert('Not implimented')
+  }
+
   return (
-    <div className="grid grid-cols-2 gap-2">
-      <FormInputDate
-        value={datePart}
-        onChange={handleOnChange}
-      />
-      <FormInputTime
-        value={timePart}
-        onChange={handleOnChange}
-      />
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-2">
+        <FormInputDate
+          name="datePart"
+          value={datePart}
+          onChange={handleOnChange}
+        />
+        <FormInputTime
+          name="timePart"
+          value={timePart}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-2 pb-0">
+        <FormInputButton
+          className="w-full"
+          text="Delete"
+          intent="DANGER"
+          onClick={initiateDelete}
+          disabled={submitting}
+        />
+        <FormInputButton
+          className="w-full"
+          text={'Save'}
+          intent='PRIMARY'
+          onClick={save}
+          disabled={submitting}
+        />
+      </div>
+    </>
   )
 }
--- pages/marketing/activity/global.tsx
+++ pages/marketing/activity/global.tsx
@@ -1,20 +1,9 @@
-import Link from 'next/link'
-import Layout from '../../../components/Layout'
-import Toolbar from "../../../components/marketing/activity/toolbar";
-import BookingDetailsForm from "../../../components/bookings/bookingDetailsForm";
-import SideMenu from "../../../components/sideMenu";
-import BookingDetailsListingPanel from "../../../components/bookings/bookingListingPanel";
-import {Show, User} from "../../../interfaces";
-import {GetStaticPaths, GetStaticProps} from "next";
-import {sampleShowData} from "../../../utils/sample-data";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
-import {ReactElement, useState} from "react";
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import MarketingPanel from "../../../components/marketing/marketing-panel";
-import GlobalActivites from "../../../components/marketing/activity/globa";
-import GlobalToolbar from 'components/toolbar';
+import Layout from 'components/Layout'
+import Toolbar from 'components/marketing/activity/toolbar'
+import { useState } from 'react'
+import GlobalActivites from 'components/marketing/activity/globa'
+import GlobalToolbar from 'components/toolbar'
+import { Show } from 'interfaces'
 
 type Props = {
     items: Show[]
