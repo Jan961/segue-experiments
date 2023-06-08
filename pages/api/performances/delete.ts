19d56bcd0c83ea401a512b476c17ff864ac9f7e6
Neil Sutcliffe <neil@neilsutcliffe.com>
2023-06-08 16:23:01 +0100

    Performance deletion

---
+++ pages/api/performances/delete.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from 'lib/prisma'
+import { PerformanceDTO } from 'interfaces'
+
+export default async function handle (req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const perf = req.body as PerformanceDTO
+    await prisma.$transaction([
+      prisma.performance.delete({
+        where: {
+          Id: perf.Id
+        }
+      })
+    ])
+    console.log(`Deleted Performance: ${perf.Id}`)
+    res.status(200).json({})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ err: 'Error Deleting Booking' })
+  }
+}
