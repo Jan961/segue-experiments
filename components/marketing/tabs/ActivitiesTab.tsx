115631c115bc2fc5aebc0b210d07ab576b75a8f7
Arun <asara97911@gmail.com>
2023-10-23 10:04:15 +0100

    test deployment

--- components/marketing/tabs/ActivitiesTab.tsx
+++ components/marketing/tabs/ActivitiesTab.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { Table } from 'components/global/table/Table'
 import { FormInputButton } from 'components/global/forms/FormInputButton'
 import axios from 'axios'
