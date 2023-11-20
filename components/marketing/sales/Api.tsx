4a139c83c85a5f72c00684a9b61dbebabdc2cd8d
Arun <asara97911@gmail.com>
2023-11-20 09:47:41 +0000

    Sales Validation fix

--- components/marketing/sales/Api.tsx
+++ components/marketing/sales/Api.tsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 export const getSales = ({ SetSalesFiguresDate, SetBookingId, isFinalFigures = false }) => {
   return axios
     .post('/api/marketing/sales/read', {
-      SetSalesFiguresDate: SetSalesFiguresDate ? moment(SetSalesFiguresDate).toISOString().split('T')[0] : null,
+      SetSalesFiguresDate: SetSalesFiguresDate ? SetSalesFiguresDate || moment(SetSalesFiguresDate).toISOString().split('T')[0] : null,
       SetBookingId,
       isFinalFigures,
     })
