d5bb69256e5ed7f9babaabdc722bede38458f75e
tom-tochito <tom@tochito.co.uk>
2023-11-21 07:41:56 +0000

    Add css utils

--- pages/bookings/[ShowCode]/[TourCode].tsx
+++ pages/bookings/[ShowCode]/[TourCode].tsx
@@ -125,8 +125,8 @@ const BookingPage = ({ TourId }: bookingProps) => {
                 <div className="col-span-1 lg:col-span-1 xl:col-span-1 p-2 whitespace-nowrap">Week No.</div>
                 <div className="col-span-4 lg:col-span-3 xl:col-span-2 p-2 whitespace-nowrap">Date</div>
                 <div className="col-span-7 lg:col-span-8 grid grid-cols-10">
-                  <div className="col-span-5 p-2">Venue</div>
-                  <div className="col-span-1 p-2">Town</div>
+                  <div className="col-span-4 p-2">Venue</div>
+                  <div className="col-span-2 p-2">Town</div>
                   <div className="col-span-1 p-2">Seats</div>
                   <div className="col-span-1 p-2">Perf(s)</div>
                   <div className="col-span-1 p-2">Miles</div>
