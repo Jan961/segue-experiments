901a532d0a31ddf0a69406b887db87bf9d15b77d
Jasmine  Ibikunle <jasmineibikunle@hotmail.co.uk>
2023-11-30 16:27:28 +0000

    wip: filters

--- components/tasks/toolbar.tsx
+++ components/tasks/toolbar.tsx
@@ -5,9 +5,12 @@ import { FormInputText } from 'components/global/forms/FormInputText';
 import { FormInputSelect } from 'components/global/forms/FormInputSelect';
 import { tourState } from 'state/tasks/tourState';
 import { useRecoilValue } from 'recoil';
+import { FormInputDate } from 'components/global/forms/FormInputDate';
+import { dateToPicker, dateToSimple, getKey } from 'services/dateService';
 
 const Toolbar: React.FC = () => {
   const [addTaskOpen, setAddTaskOpen] = React.useState(false);
+  const [addRecurringTaskOpen, setAddRecurringTaskOpen] = React.useState(false);
   const [filters, setFilters] = React.useState({ Search: '', Tour: undefined });
   const tours = useRecoilValue(tourState);
 
@@ -25,7 +28,6 @@ const Toolbar: React.FC = () => {
   return (
     <div>
       <div className="flex flex-row gap-2 justify-end items-center">
-        <FormInputText placeholder="Search" onChange={handleOnChange} name="Search" value={filters.Search} />
         <FormInputSelect
           className="w-80"
           onChange={handleOnChange}
@@ -33,14 +35,58 @@ const Toolbar: React.FC = () => {
           value={filters.Tour}
           options={tourOptions}
         />
-        <ToolbarButton className="mb-2" onClick={() => setAddTaskOpen(true)}>
-          Add Task
+        <div className='flex gap-2'>
+          <ToolbarButton className="mb-2" onClick={() => setAddRecurringTaskOpen(true)}>
+            Add Recurring Task
+          </ToolbarButton>
+          <ToolbarButton className="mb-2" onClick={() => setAddTaskOpen(true)}>
+            Add Task
+          </ToolbarButton>
+          <ToolbarButton className="mb-2" onClick={() => setFilters({ Search: '', Tour: undefined })}>
+            Show All
+          </ToolbarButton>
+          <ToolbarButton className="mb-2" onClick={() => setAddTaskOpen(true)}>
+            Report
+          </ToolbarButton>
+          <FormInputText placeholder="Search" onChange={handleOnChange} name="Search" value={filters.Search} />
+        </div>
+      </div>
+      <div className="flex flex-row gap-2">
+        <div className='flex'>
+          <p>Due date</p>
+          {/* <FormInputDate name="Date" label="Date" onChange={handleOnChange} /> */}
+        </div>
+        <div className='flex'>
+          <p>to</p>
+        </div>
+        <div className='flex'>
+          <p>Asignee</p>
+          <FormInputSelect
+            className="w-80"
+            onChange={handleOnChange}
+            name="Tour"
+            value={filters.Tour}
+            options={tourOptions}
+          />
+        </div>
+        <div className='flex'>
+          <p>Status</p>
+          <FormInputSelect
+            className="w-80"
+            onChange={handleOnChange}
+            name="Tour"
+            value={filters.Tour}
+            options={tourOptions}
+          />
+        </div>
+        <ToolbarButton className="mb-2" onClick={() => applyFilters()}>
+          Submit
         </ToolbarButton>
+
+      </div>
+      <div>
         {addTaskOpen && <TaskEditor open={addTaskOpen} triggerClose={() => setAddTaskOpen(false)} />}
-        {/*
-          <ToolbarButton onClick={() => setAddRecurringTaskOpen(true)}>Add Recurring Task</ToolbarButton>
-          { addRecurringTaskOpen && (<TaskEditor recurring open={addRecurringTaskOpen} triggerClose={() => setAddRecurringTaskOpen(false)} tours={tours} />)}
-        */}
+        {addRecurringTaskOpen && (<TaskEditor recurring open={addRecurringTaskOpen} triggerClose={() => setAddRecurringTaskOpen(false)} tours={tours} />)}
       </div>
     </div>
   );
