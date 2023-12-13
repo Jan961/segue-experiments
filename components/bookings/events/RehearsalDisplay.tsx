3738b58a918ae08ada03dc1c524852635fbd870a
tom-tochito <tom@tochito.co.uk>
2023-12-13 09:48:50 +0000

    Update RehearsalDisplay.tsx

--- components/bookings/events/RehearsalDisplay.tsx
+++ components/bookings/events/RehearsalDisplay.tsx
@@ -19,15 +19,11 @@ export const RehearsalDisplay = ({ rehearsalId, date }: RehearsalDisplayProps) =
     setView({ ...view, selected: { type: 'rehearsal', id: rehearsalId }, selectedDate: date });
   };
 
-  const active = view.selected?.id === rehearsalId && view.selected?.type === 'rehearsal';
-
   return (
     <div
       className={`p-1 px-2 rounded
         border border-l-8 
-        border-red-500 bg-red-200
         grid grid-cols-10
-        ${active ? 'bg-red-400 shadow' : 'hover:bg-red-300'}
        `}
       onClick={select}
     >
