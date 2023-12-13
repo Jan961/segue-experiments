698ab44241af28106cf7bb8f4fc3c50a1d9311af
tom-tochito <tom@tochito.co.uk>
2023-12-13 22:15:56 +0000

    Update OtherDisplay.tsx

--- components/bookings/OtherDisplay.tsx
+++ components/bookings/OtherDisplay.tsx
@@ -28,12 +28,8 @@ export const OtherDisplay = ({ otherId, date }: OtherDisplayProps) => {
   return (
     <div
       onClick={select}
-      className={`p-1 px-2 border-l-8
-      border rounded
-      grid grid-cols-10
-      border-lime-500 bg-lime-200
-      ${active ? 'shadow bg-lime-400' : 'hover:bg-lime-300'}
-      `}
+      className={`${active ? 'bg-primary-blue text-white' : ''} 
+      p-1 px-2 rounded grid grid-cols-10 cursor-pointer`}
     >
       <div className="col-span-7 text-center">{match?.Name}</div>
     </div>
