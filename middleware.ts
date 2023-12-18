43ed029381d4a49fa240e57cfcc78c5a89f9da48
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2023-12-18 08:27:24 +0000

    Rolled back dev changes

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
 import { Clerk } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import { authMiddleware } from '@clerk/nextjs';
-<<<<<<< HEAD
 import prismaAccelerate from 'lib/prismaAccelerate';
-=======
-// import prismaEdge from 'lib/prismaEdge';
->>>>>>> f947822 (Disabled prisma proxy)
 
 const publicPaths = ['/sign-in*', '/sign-up*', '/access-denied'];
 
@@ -31,17 +27,12 @@ export default authMiddleware({
     if (userId) {
       const user = await clerk.users.getUser(userId);
       const userEmail = user.emailAddresses.find((email) => email.id === user.primaryEmailAddressId).emailAddress;
-      console.log(userEmail);
       /*
         Uses MIDDLEWARE_DATABASE_URL in .env
         This is as standard database calls don't work on Vercel Edge. This middleware flagged an error if you tried a lookup
         https://www.prisma.io/docs/data-platform/classic-projects/data-proxy/use-data-proxy#enable-the-data-proxy-for-a-project
       */
-<<<<<<< HEAD
       const access = await prismaAccelerate.user.findUnique({ where: { Email: userEmail }, select: { Id: true } });
-=======
-      const access = true; // await prismaEdge.user.findUnique({ where: { Email: userEmail }, select: { Id: true } });
->>>>>>> f947822 (Disabled prisma proxy)
 
       if (access) return NextResponse.next();
       const denied = new URL('/access-denied', request.url);
