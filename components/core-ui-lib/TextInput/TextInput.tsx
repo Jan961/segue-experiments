9a5e8c57e94e3e7b0a85c3fedb7fc8061b443631
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-02-05 16:35:48 +0000

    DateInput refactoring and grid styling changes

--- components/core-ui-lib/TextInput/TextInput.tsx
+++ components/core-ui-lib/TextInput/TextInput.tsx
@@ -12,28 +12,17 @@ export interface TextInputProps {
   placeHolder?: string;
   onClick?: (e: any) => void;
   iconName?: IconName;
-  onKeyDown?: (e: any) => void;
   error?: string;
+  onFocus: (e: any) => void;
+  onBlur: (e: any) => void;
 }
 
 const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
   (
-    {
-      id,
-      value = '',
-      className = '',
-      disabled = false,
-      onChange,
-      placeHolder = '',
-      onClick,
-      iconName,
-      onKeyDown,
-      error,
-    },
+    { id, value = '', className = '', disabled = false, onChange, placeHolder = '', onClick, iconName, error, ...rest },
     ref,
-    ...rest
   ) => {
-    const baseClass = `block min-w-fit h-[1.9375rem] !border text-sm shadow-inner text-primary-input-text rounded-md outline-none focus:ring-2 focus:ring-primary-input-text ring-inset`;
+    const baseClass = `block w-fit-content pl-2 h-[1.9375rem] !border text-sm shadow-inner text-primary-input-text rounded-md outline-none focus:ring-2 focus:ring-primary-input-text ring-inset`;
     const inputClass = error ? '!border-primary-red' : '!border-primary-border';
     const disabledClass = disabled ? `!bg-disabled !cursor-not-allowed !pointer-events-none` : '';
 
@@ -43,16 +32,15 @@ const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
           ref={ref}
           id={id}
           type="text"
-          className={classNames(baseClass, inputClass, disabledClass, `${iconName ? 'pr-10' : ''}`, className)}
+          className={classNames(baseClass, inputClass, disabledClass, `${iconName ? 'pr-6' : ''}`, className)}
           disabled={disabled}
           onChange={onChange}
           placeholder={placeHolder}
           value={value || ''}
           {...rest}
-          onKeyDown={onKeyDown}
         />
         {iconName && (
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <Icon aria-hidden="true" iconName={iconName} />
           </div>
         )}
