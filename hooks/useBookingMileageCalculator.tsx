af30b3e9db821f1b83c9953b2c7a9acc0047cc00
Arun <asara97911@gmail.com>
2024-02-06 03:28:24 +0000

    mileage calculator trigger when production change

--- hooks/useBookingMileageCalculator.tsx
+++ hooks/useBookingMileageCalculator.tsx
@@ -3,10 +3,12 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { bookingState } from 'state/booking/bookingState';
 import { distanceState } from 'state/booking/distanceState';
+import { productionJumpState } from 'state/booking/productionJumpState';
 import { getStops } from 'utils/getStops';
 
 const useMileageCalculator = () => {
   const [distance, setDistance] = useRecoilState(distanceState);
+  const { selected } = useRecoilValue(productionJumpState);
   const bookingDict = useRecoilValue(bookingState);
   const [loading, setLoading] = useState(false);
   const shouldRefresh = useMemo(() => !loading && Object.values(distance).some((x) => x.outdated), [distance]);
@@ -14,8 +16,10 @@ const useMileageCalculator = () => {
     const stops = getStops(bookingDict);
     setLoading(true);
     const promises = Object.keys(stops).map(async (prodId) => {
-      const { data } = await axios.post('/api/distance', stops[prodId]);
-      setDistance({ ...distance, [prodId]: { stops: data, outdated: false } });
+      if (distance[prodId].outdated) {
+        const { data } = await axios.post('/api/distance', stops[prodId]);
+        setDistance({ ...distance, [prodId]: { stops: data, outdated: false } });
+      }
     });
     await Promise.allSettled(promises);
     setLoading(false);
@@ -23,7 +27,7 @@ const useMileageCalculator = () => {
   useEffect(() => {
     if (!shouldRefresh) return;
     refresh();
-  }, []);
+  }, [selected]);
   return { loading, distance };
 };
 
