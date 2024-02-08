c7fc454dabeecf971e4fb58fd8f3807e67b80094
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-02-08 14:55:04 +0000

    Fix for ISO and local dates

--- hooks/useBookingsFilter.ts
+++ hooks/useBookingsFilter.ts
@@ -9,13 +9,23 @@ const useBookingFilter = () => {
   const { selected } = useRecoilValue(productionJumpState);
   const { rows } = useRecoilValue(rowsSelector);
 
+  const tz = useMemo(() => filter?.endDate?.getTimezoneOffset() * 60000 || 0, [filter]);
+
+  const compareUTCAndLocalDates = (utcDate: Date, localDate: Date, tzOffset: number, comparator: string) => {
+    if (comparator === 'lte') {
+      return utcDate.getTime() + tzOffset <= localDate.getTime();
+    } else if (comparator === 'gte') {
+      return utcDate.getTime() - tzOffset >= localDate.getTime();
+    }
+  };
+
   const filteredRows = useMemo(() => {
     const filteredRowList = rows.filter(({ dateTime, status, productionId, venue, town }) => {
       console.log(dateTime, !filter.endDate || new Date(dateTime) <= filter.endDate);
       return (
         (selected === -1 || productionId === selected) &&
-        (!filter.endDate || new Date(dateTime) <= filter.endDate) &&
-        (!filter.startDate || new Date(dateTime) >= filter.startDate) &&
+        (!filter.endDate || compareUTCAndLocalDates(new Date(dateTime), filter.endDate, tz, 'lte')) &&
+        (!filter.startDate || compareUTCAndLocalDates(new Date(dateTime), filter.startDate, tz, 'gte')) &&
         (filter.status === 'all' || status === filter.status) &&
         (!filter.venueText ||
           venue?.toLowerCase?.().includes?.(filter.venueText?.toLowerCase()) ||
