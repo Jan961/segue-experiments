2212db29ea7ddddf3ff19d417a63f1a456515196
Arun <asara97911@gmail.com>
2024-02-12 16:03:14 +0000

    added reducer

---
+++ config/AddBooking.ts
@@ -0,0 +1,13 @@
+export const INITIAL_STATE = {
+  form: {
+    fromDate: '',
+    toDate: '',
+    isDateTypeOnly: false,
+    dateType: null,
+    venueId: null,
+  },
+};
+
+export const Actions = {
+  UPDATE_FORM_DATA: 'UPDATE_FORM_DATA',
+};
