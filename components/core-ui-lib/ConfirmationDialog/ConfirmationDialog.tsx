80f8d57349d9fafbb6d83c3d0c63d414697cb1d4
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-02-13 16:57:58 +0000

    as per comments, component is now inheriting the PopupModal

--- components/core-ui-lib/ConfirmationDialog/ConfirmationDialog.tsx
+++ components/core-ui-lib/ConfirmationDialog/ConfirmationDialog.tsx
@@ -1,6 +1,6 @@
-import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import Button from '../Button';
+import PopupModal from '../PopupModal';
 
 type ConfDialogVariant = 'close' | 'cancel' | 'delete' | 'logout' | 'leave';
 type ModalText = {
@@ -29,9 +29,9 @@ export default function ConfirmationDialog({
 
 
   // set text based on  variant
-  const text: ModalText = { question: '', warning: ''};
+  const text: ModalText = { question: '', warning: '' };
 
-  switch(variant) {
+  switch (variant) {
     case 'close': { text.question = 'Are you sure you want to close?'; text.warning = 'Any unsaved changes may be lost.'; }
     case 'cancel': { text.question = 'Are you sure you want to cancel?'; text.warning = 'Any unsaved changes may be lost.'; }
     case 'delete': { text.question = 'Are you sure you want to delete?'; text.warning = 'This action cannot be undone.'; }
@@ -42,47 +42,18 @@ export default function ConfirmationDialog({
   const [dialogVisible, setVisible] = useState(show);
 
   return (
-    <Transition appear show={dialogVisible} as={Fragment}>
-      <Dialog as="div" className="relative z-150" onClose={() => null}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black/75 z-10" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-y-auto z-20">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="px-7 pt-2 transform bg-primary-white text-left align-middle shadow-xl transition-all">
-                <div>
-                  <div className="text-center">
-                    <div className='text text-primary-navy font-bold text-xl'>{text.question}</div>
-                    <div className='text text-primary-navy font-bold text-xl'>{text.warning}</div>
-                  </div>
-                  <div className="w-full mt-4 flex justify-center items-center mb-4">
-                    <Button className='w-32' variant='secondary' text={labelNo} onClick={() => setVisible(false)} />
-                    <Button className={variant === 'delete' ? 'ml-4 w-32 bg-primary-red' : 'ml-4 w-32'}  text={labelYes} onClick={() => { onYesClick(); setVisible(false) }} />
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
+    <PopupModal
+      show={dialogVisible}
+      showCloseIcon={false}
+    >
+      <div className="text-center">
+        <div className='text text-primary-navy font-bold text-xl'>{text.question}</div>
+        <div className='text text-primary-navy font-bold text-xl'>{text.warning}</div>
+      </div>
+      <div className="w-full mt-4 flex justify-center items-center mb-4">
+        <Button className='w-32' variant='secondary' text={labelNo} onClick={() => setVisible(false)} />
+        <Button className={variant === 'delete' ? 'ml-4 w-32 bg-primary-red' : 'ml-4 w-32'} text={labelYes} onClick={() => { onYesClick(); setVisible(false) }} />
+      </div>
+    </PopupModal>
   );
 }
