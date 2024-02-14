1a36106f432ab04da25454fcbcc9179785434533
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-02-14 14:50:22 +0000

    added api call to edit the note, changed the modal design as per ticket SK-25

--- components/bookings/BookingsTable.tsx
+++ components/bookings/BookingsTable.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import NotesPopup from './NotesPopup';
 import { useRecoilState } from 'recoil';
 import { filterState } from 'state/booking/filterState';
+import axios from 'axios';
 
 interface BookingsTableProps {
   rowData?: any;
@@ -39,8 +40,17 @@ export default function BookingsTable({ rowData }: BookingsTableProps) {
   };
 
   const handleSaveNote = (value) => {
-    console.log(value);
     setShowModal(false);
+
+    // SK-25 PL - run update booking to add the new note
+    axios
+      .patch('/api/bookings/update/', { BookingId: productionItem.Id, Notes: value })
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        alert(error);
+      });
   };
 
   const handleCancelNote = () => {
@@ -118,7 +128,7 @@ export default function BookingsTable({ rowData }: BookingsTableProps) {
       </div>
       <NotesPopup
         show={showModal}
-        value={productionItem?.note || ''}
+        productionItem={productionItem}
         onSave={handleSaveNote}
         onCancel={handleCancelNote}
       />
