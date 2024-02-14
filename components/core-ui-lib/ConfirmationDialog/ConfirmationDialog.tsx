7e3120b63980ebf3461523770c8f1dbcd1aec2fb
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-02-14 10:42:57 +0000

    negative margin top added to satifiy design amendment from @cheryl

--- components/core-ui-lib/ConfirmationDialog/ConfirmationDialog.tsx
+++ components/core-ui-lib/ConfirmationDialog/ConfirmationDialog.tsx
@@ -6,7 +6,7 @@ type ConfDialogVariant = 'close' | 'cancel' | 'delete' | 'logout' | 'leave';
 type ModalText = {
   question: string;
   warning: string;
-}
+};
 
 interface ConfirmationDialogProps {
   children?: React.ReactNode;
@@ -24,35 +24,61 @@ export default function ConfirmationDialog({
   onYesClick,
   labelYes = 'Yes',
   labelNo = 'No',
-  variant
+  variant,
 }: ConfirmationDialogProps) {
-
-
   // set text based on  variant
   const text: ModalText = { question: '', warning: '' };
 
   switch (variant) {
-    case 'close': { text.question = 'Are you sure you want to close?'; text.warning = 'Any unsaved changes may be lost.'; }
-    case 'cancel': { text.question = 'Are you sure you want to cancel?'; text.warning = 'Any unsaved changes may be lost.'; }
-    case 'delete': { text.question = 'Are you sure you want to delete?'; text.warning = 'This action cannot be undone.'; }
-    case 'logout': { text.question = 'Are you sure you want to logout?'; text.warning = ''; }
-    case 'leave': { text.question = 'Are you sure you want to leave this page?'; text.warning = 'Any unsaved changes may be lost.'; }
+    case 'close': {
+      text.question = 'Are you sure you want to close?';
+      text.warning = 'Any unsaved changes may be lost.';
+      break;
+    }
+
+    case 'cancel': {
+      text.question = 'Are you sure you want to cancel?';
+      text.warning = 'Any unsaved changes may be lost.';
+      break;
+    }
+
+    case 'delete': {
+      text.question = 'Are you sure you want to delete?';
+      text.warning = 'This action cannot be undone.';
+      break;
+    }
+
+    case 'logout': {
+      text.question = 'Are you sure you want to logout?';
+      text.warning = '';
+      break;
+    }
+
+    case 'leave': {
+      text.question = 'Are you sure you want to leave this page?';
+      text.warning = 'Any unsaved changes may be lost.';
+      break;
+    }
   }
 
   const [visible, setVisible] = useState<boolean>(show);
 
   return (
-    <PopupModal
-      show={visible}
-      showCloseIcon={false}
-    >
-      <div className="text-center">
-        <div className='text text-primary-navy font-bold text-xl'>{text.question}</div>
-        <div className='text text-primary-navy font-bold text-xl'>{text.warning}</div>
-      </div>
-      <div className="w-full mt-4 flex justify-center items-center mb-4">
-        <Button className='w-32' variant='secondary' text={labelNo} onClick={() => setVisible(false)} />
-        <Button className='ml-4 w-32' variant={variant === 'delete' ? 'tertiary' : 'primary'} text={labelYes} onClick={onYesClick} />
+    <PopupModal show={visible} showCloseIcon={false}>
+      <div className="-mt-5">
+        <div className="text-center">
+          <div className="text text-primary-navy font-bold text-xl">{text.question}</div>
+          <div className="text text-primary-navy font-bold text-xl">{text.warning}</div>
+        </div>
+        <div className="w-full mt-4 flex justify-center items-center mb-4">
+          <Button className="w-32" variant="secondary" text={labelNo} onClick={() => setVisible(false)} />
+          <Button
+            className="ml-4 w-32"
+            variant={variant === 'delete' ? 'tertiary' : 'primary'}
+            text={labelYes}
+            onClick={onYesClick}
+          />
+        </div>
       </div>
     </PopupModal>
   );
