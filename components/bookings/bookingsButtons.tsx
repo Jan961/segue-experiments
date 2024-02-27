5a909e1737be0fa4f94d3bf53f918171ffee053a
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-02-27 11:04:01 +0000

    Merged main

--- components/bookings/bookingsButtons.tsx
+++ components/bookings/bookingsButtons.tsx
@@ -1,23 +1,57 @@
 import Barring from './modal/barring';
 import Button from 'components/core-ui-lib/Button';
 import AddBooking from './modal/NewBooking';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { currentProductionSelector } from 'state/booking/selectors/currentProductionSelector';
+import Tooltip from 'components/core-ui-lib/Tooltip';
+import { productionJumpState } from 'state/booking/productionJumpState';
 
 export default function BookingsButtons() {
-  const [showAddNewBookingModal, setShowAddNewBookingModal] = useState(false);
-  const [showBarringModal, setShowBarringModal] = useState(false);
+  const [showAddNewBookingModal, setShowAddNewBookingModal] = useState<boolean>(false);
+  const [showBarringModal, setShowBarringModal] = useState<boolean>(false);
+  const [bookingsDisabled, setBookingsDisabled] = useState<boolean>(false);
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const production = useRecoilValue(currentProductionSelector);
+  const { selected: ProductionId } = useRecoilValue(productionJumpState);
+
+  useEffect(() => {
+    setBookingsDisabled(production === undefined || production.IsArchived === true);
+    setDisabled(!ProductionId);
+  }, [production, ProductionId]);
+
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-4">
-      <Button text="Create New Booking" className="w-[155px]" onClick={() => setShowAddNewBookingModal(true)}></Button>
+      <Tooltip
+        body="Please select a current Production"
+        position="left"
+        width="w-44"
+        // disable tooltip when Production Dropdown = "Please select a production"
+        manualToggle={!ProductionId ? false : bookingsDisabled}
+        useManualToggle={!ProductionId ? true : !bookingsDisabled}
+      >
+        <Button
+          disabled={bookingsDisabled}
+          text="Create New Booking"
+          className="w-[155px]"
+          onClick={() => setShowAddNewBookingModal(true)}
+        ></Button>
+      </Tooltip>
       <Button
+        disabled={disabled}
         text="Booking Reports"
         className="w-[155px]"
         iconProps={{ className: 'h-4 w-3' }}
         sufixIconName={'excel'}
       ></Button>
-      <Button text="Venue History" className="w-[155px]"></Button>
+      <Button disabled={disabled} text="Venue History" className="w-[155px]"></Button>
 
-      <Button text="Barring Check" className="w-[155px]" onClick={() => setShowBarringModal(true)}></Button>
+      <Button
+        disabled={disabled}
+        text="Barring Check"
+        className="w-[155px]"
+        onClick={() => setShowBarringModal(true)}
+      ></Button>
 
       {showBarringModal && <Barring visible={showBarringModal} onClose={() => setShowBarringModal(false)} />}
       {showAddNewBookingModal && (
--- components/core-ui-lib/Tooltip/Tooltip.tsx
+++ components/core-ui-lib/Tooltip/Tooltip.tsx
@@ -39,49 +39,27 @@ const Tooltip: React.FC<TooltipProps> = ({
     }
   };
 
-  const getPositionStyle = () => {
-    switch (position) {
-      case 'top':
-        return 'bottom-full left-1/2 transform -translate-x-1/2 mb-3';
-      case 'bottom':
-        return 'top-full left-1/2 transform -translate-x-1/2 mt-3';
-      case 'left':
-        return 'top-1/2 right-full transform -translate-y-1/2 mr-4';
-      case 'right':
-        return 'top-1/2 left-full transform -translate-y-1/2 ml-4';
-      default:
-        return 'bottom-full left-1/2 transform -translate-x-1/2 mb-3';
-    }
+  const positionStyle = {
+    top: 'bottom-full left-1/2 transform -translate-x-1/2 mb-3',
+    bottom: 'top-full left-1/2 transform -translate-x-1/2 mt-3',
+    left: 'top-1/2 right-full transform -translate-y-1/2 mr-4',
+    right: 'top-1/2 left-full transform -translate-y-1/2 ml-4',
   };
 
-  const getArrowStyle = () => {
-    switch (position) {
-      case 'top':
-        return (
-          'absolute top-0 left-1/2 -mt-3 -ml-2 w-0 h-0 border-t-[10px] border-l-[10px] border-r-[10px] border-transparent border-t-' +
-          bgColorClass
-        );
-      case 'bottom':
-        return (
-          'absolute bottom-0 left-1/2 -mb-3 -ml-2 w-0 h-0 border-b-[10px] border-l-[10px] border-r-[10px] border-transparent border-b-' +
-          bgColorClass
-        );
-      case 'right':
-        return (
-          'absolute top-1/2 right-0 -mr-4 -mt-2 w-0 h-0 border-t-[10px] border-b-[10px] border-r-[10px] border-transparent border-r-' +
-          bgColorClass
-        );
-      case 'left':
-        return (
-          'absolute top-1/2 left-0 -ml-4 -mt-2 w-0 h-0 border-t-[10px] border-b-[10px] border-l-[10px] border-transparent border-l-' +
-          bgColorClass
-        );
-      default:
-        return (
-          'absolute top-0 left-1/2 -mt-3 -ml-2 w-0 h-0 border-t-[10px] border-l-[10px] border-r-[10px] border-transparent border-t-' +
-          bgColorClass
-        );
-    }
+  const arrowStyle = {
+    top: 'absolute top-0 left-1/2 -mt-3 -ml-2 w-0 h-0 border-t-[10px] border-l-[10px] border-r-[10px] border-transparent',
+    bottom:
+      'absolute bottom-0 left-1/2 -mb-3 -ml-2 w-0 h-0 border-b-[10px] border-l-[10px] border-r-[10px] border-transparent',
+    right:
+      'absolute top-1/2 right-0 -mr-4 -mt-2 w-0 h-0 border-t-[10px] border-b-[10px] border-r-[10px] border-transparent',
+    left: 'absolute top-1/2 left-0 -ml-4 -mt-2 w-0 h-0 border-t-[10px] border-b-[10px] border-l-[10px] border-transparent',
+  };
+
+  const arrowBg = {
+    top: 'border-t-' + bgColorClass,
+    bottom: 'border-b-' + bgColorClass,
+    right: 'border-r-' + bgColorClass,
+    left: 'border-l-primary-navy', // + bgColorClass,
   };
 
   return (
@@ -91,9 +69,13 @@ const Tooltip: React.FC<TooltipProps> = ({
       </div>
       {showTooltip && (
         <div className="items-center">
-          <div className={`${bgColorClass} ${getArrowStyle()}`}></div>
           <div
-            className={`absolute z-10 p-2 ${txtColorClass} bg-${bgColorClass} p-4 rounded-md ${getPositionStyle()} ${height} ${width}`}
+            className={`${bgColorClass} ${arrowStyle[position] || arrowStyle.top} ${arrowBg[position] || arrowBg.top}`}
+          ></div>
+          <div
+            className={`absolute z-10 p-2 ${txtColorClass} bg-${bgColorClass} p-4 rounded-md ${
+              positionStyle[position] || positionStyle.top
+            } ${height} ${width}`}
           >
             <div className="relative text-center items-center h-full">
               <div className="text font-bold leading-[1.125]">{title}</div>
