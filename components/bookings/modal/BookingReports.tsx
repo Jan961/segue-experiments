637bc61420b80c7f3328879642357751c4ba187d
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-03-01 09:53:47 +0000

    removed btn height set locally

--- components/bookings/modal/BookingReports.tsx
+++ components/bookings/modal/BookingReports.tsx
@@ -26,28 +26,28 @@ export const BookingReports = ({ visible = false, onClose }: BookingReportProps)
       <div className="-mb-1">
         <Button
           text="Tour Schedule"
-          className="w-[230px] h-[30px] mb-3 pl-6"
+          className="w-[230px] mb-3 pl-6"
           iconProps={{ className: 'h-4 w-3 ml-5' }}
           sufixIconName={'excel'}
         ></Button>
 
         <Button
           text="Travel Summary"
-          className="w-[230px] h-[30px] mb-3 pl-6"
+          className="w-[230px] mb-3 pl-6"
           iconProps={{ className: 'h-4 w-3 ml-5' }}
           sufixIconName={'excel'}
         ></Button>
 
         <Button
           text="All Productions Masterplan"
-          className="w-[230px] h-[30px] mb-3 pl-5"
+          className="w-[230px] mb-3 pl-5"
           iconProps={{ className: 'h-4 w-3 ml-5' }}
           sufixIconName={'excel'}
         ></Button>
 
         <Button
           text="Multiple Pencil Report"
-          className="w-[230px] h-[30px] mb-3 pl-6"
+          className="w-[230px] mb-3 pl-6"
           iconProps={{ className: 'h-4 w-3 ml-5' }}
           sufixIconName={'excel'}
         ></Button>
