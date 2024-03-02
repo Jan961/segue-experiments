fd7777850450b9f40ffd404b7357dd8dcce8a496
Huzaifa-31 <31huzafasameer@gmail.com>
2024-03-02 06:08:20 +0500

    preview and mileage refactor

--- components/bookings/modal/NewBooking/views/MileageBooking.tsx
+++ components/bookings/modal/NewBooking/views/MileageBooking.tsx
@@ -8,26 +8,11 @@ import { rowsSelector } from 'state/booking/selectors/rowsSelector';
 import { getDateDaysAgo, getDateDaysInFuture, toSql } from 'services/dateService';
 import moment from 'moment';
 import { venueState } from 'state/booking/venueState';
-// import { dateTypeState } from 'state/booking/dateTypeState';
-import { distanceState } from 'state/booking/distanceState';
+// import { distanceState } from 'state/booking/distanceState';
 import { bookingStatusMap } from 'config/bookings';
 import { SelectOption } from 'components/core-ui-lib/Select/Select';
 import { steps } from 'config/AddBooking';
-// type PreviewDataItem = {
-//   production: string;
-//   date: string;
-//   week: string;
-//   venue: number;
-//   town: string;
-//   perf: boolean;
-//   dayType: string;
-//   bookingStatus: string;
-//   capacity: number;
-//   noPerf: number;
-//   perfTimes: string;
-//   miles: string;
-//   travelTime: string;
-// };
+
 type NewBookingDetailsProps = {
   formData: TForm;
   productionCode: string;
@@ -36,10 +21,9 @@ type NewBookingDetailsProps = {
 };
 export default function MileageBooking({ formData, productionCode, data, dayTypeOptions }: NewBookingDetailsProps) {
   const venueDict = useRecoilValue(venueState);
-  const distanceDict = useRecoilValue(distanceState);
-  console.log('distanceDict :>> ', distanceDict);
-  console.log('venueDict :>> ', venueDict);
-  const updateData = data.map((item: any) => ({
+  //   const distanceDict = useRecoilValue(distanceState);
+
+  const updateData: any = data.map((item: any) => ({
     ...item,
 
     venue: venueDict[item.venue].Name,
@@ -51,30 +35,21 @@ export default function MileageBooking({ formData, productionCode, data, dayType
     performanceCount: item.noPerf?.toString() || '',
     performanceTimes: item.times,
   }));
-  console.log('updateData :>> ', updateData);
 
-  console.log('data :>> ', data);
   const { rows: bookings } = useRecoilValue(rowsSelector);
 
-  console.log('rows selector :>> ', bookings);
-  console.log('data to preview booking :>> ', formData);
   const { fromDate, toDate } = formData;
 
   const sqlFromDate = toSql(fromDate);
   const sqlToDate = toSql(toDate);
-  console.log('sqlFromDate :>> ', sqlFromDate);
-  console.log('sqlToDate :>> ', sqlToDate);
+
   const pastStartDate = getDateDaysAgo(sqlFromDate, 5);
   const futureEndDate = getDateDaysInFuture(sqlToDate, 5);
 
   const pastStartDateP = moment(pastStartDate).format('YYYY-MM-DD');
   const pastStartDateF = moment(futureEndDate).format('YYYY-MM-DD');
-  console.log('pastStartDate,,, :>> ', pastStartDateP);
-  console.log('Future End Date:,,,', pastStartDateF);
 
   const filterBookingsByDateRange = (bookings, startDate, endDate) => {
-    console.log('startDate-- :>> ', startDate);
-    console.log('endDate-- :>> ', endDate);
     const filteredBookings = [];
     bookings.forEach((booking) => {
       const bookingDate = booking.dateTime;
@@ -94,7 +69,7 @@ export default function MileageBooking({ formData, productionCode, data, dayType
 
       return dateA - dateB;
     });
-    // return filteredBookings;
+
     return sortedFilteredBookings;
   };
 
@@ -102,73 +77,6 @@ export default function MileageBooking({ formData, productionCode, data, dayType
   const filteredBookingsBottom = filterBookingsByDateRange(bookings, sqlToDate, pastStartDateF);
   const mergedFilteredBookings = [...filteredBookingsTop, ...updateData, ...filteredBookingsBottom];
 
-  console.log('filter booking ,,,', filteredBookingsTop);
-  console.log('filteredBookingsBottom :>> ', filteredBookingsBottom);
-  console.log('mergedFilteredBookings :>> ', mergedFilteredBookings);
-
-  //   const dummyData: PreviewDataItem[] = [
-  //     {
-  //       production: '1',
-  //       date: '2024-01-01',
-  //       week: 'Week 1',
-  //       venue: 1,
-  //       town: 'City 1',
-  //       perf: true,
-  //       dayType: 'Performance',
-  //       bookingStatus: 'Confirmed',
-  //       capacity: 1000,
-  //       noPerf: 2,
-  //       perfTimes: '7:00 PM - 9:00 PM',
-  //       miles: '50',
-  //       travelTime: '1 hour',
-  //     },
-  //     {
-  //       production: '2',
-  //       date: '2024-01-02',
-  //       week: 'Week 1',
-  //       venue: 2,
-  //       town: 'City 2',
-  //       perf: false,
-  //       dayType: 'Rehearsal',
-  //       bookingStatus: 'Pending',
-  //       capacity: 800,
-  //       noPerf: 0,
-  //       perfTimes: '',
-  //       miles: '30',
-  //       travelTime: '45 minutes',
-  //     },
-  //     {
-  //       production: '3',
-  //       date: '2024-01-03',
-  //       week: 'Week 1',
-  //       venue: 3,
-  //       town: 'City 3',
-  //       perf: true,
-  //       dayType: 'Performance',
-  //       bookingStatus: 'Confirmed',
-  //       capacity: 1200,
-  //       noPerf: 1,
-  //       perfTimes: '6:30 PM - 8:30 PM',
-  //       miles: '40',
-  //       travelTime: '50 minutes',
-  //     },
-  //     {
-  //       production: '4',
-  //       date: '2024-01-04',
-  //       week: 'Week 1',
-  //       venue: 4,
-  //       town: 'City 4',
-  //       perf: false,
-  //       dayType: 'Travel Day',
-  //       bookingStatus: 'Confirmed',
-  //       capacity: 900,
-  //       noPerf: 0,
-  //       perfTimes: '',
-  //       miles: '60',
-  //       travelTime: '1.5 hours',
-  //     },
-  //     // Add more dummy data as needed
-  //   ];
   const { goToStep } = useWizard();
   const goToNewBookingDetail = () => {
     goToStep(steps.indexOf('New Booking Details'));
