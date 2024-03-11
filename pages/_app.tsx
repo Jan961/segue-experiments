8a1edf68333a1635b30258c0a73ca72ae946b0cb
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-03-11 20:11:01 +0000

    Add ag-grid-enterprise

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import '../styles/globals.css';
 import 'react-range-slider-input/dist/style.css';
 import 'react-datepicker/dist/react-datepicker.css';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-quartz.css';
-
+import 'ag-grid-enterprise/styles/ag-grid.css';
+import 'ag-grid-enterprise/styles/ag-theme-quartz.css';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
@@ -13,6 +12,7 @@ import { ClerkProvider, SignedIn } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
 import TanstackProvider from 'components/providers/TanstackProvider';
 import ReferenceDataLoader from 'components/ReferenceDataLoader';
+import { LicenseManager } from 'ag-grid-enterprise';
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -24,6 +24,7 @@ type AppPropsWithLayout = AppProps & {
 
 // Login disabled due to lack of support. DO NOT PUT PUBLIC
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  LicenseManager.setLicenseKey(process.env.NEXT_PUBLIC_AG_GRID_LICENCE_KEY);
   const { initialState } = pageProps;
   const router = useRouter();
 
