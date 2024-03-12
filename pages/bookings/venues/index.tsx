99d66d7b96bf8a49e51fc825e3127406bc8e9968
Huzaifa-31 <31huzafasameer@gmail.com>
2024-03-12 17:59:04 +0500

    change branch

--- pages/bookings/venues/index.tsx
+++ pages/bookings/venues/index.tsx
@@ -1,14 +1,18 @@
 import Layout from 'components/Layout';
 import VenueFilter from 'components/venues/VenueFilter';
 import VenueTable from 'components/venues/VenueTable';
+import AddEditVenueModal from 'components/venues/modal/AddEditVenueModal';
 
 export default function index() {
   return (
-    <Layout title="Venues | Segue" flush>
-      <div className="mb-8">
-        <VenueFilter />
-      </div>
-      <VenueTable />
-    </Layout>
+    <>
+      <Layout title="Venues | Segue" flush>
+        <div className="mb-8">
+          <VenueFilter />
+        </div>
+        <VenueTable />
+      </Layout>
+      <AddEditVenueModal />
+    </>
   );
 }
