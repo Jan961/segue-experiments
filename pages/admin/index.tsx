a784a7e15026a00ef07ba98e0e7adc43c512ed5c
Huzaifa-31 <31huzafasameer@gmail.com>
2024-03-18 22:45:58 +0500

    Tiles componet created

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,5 @@
 import Layout from 'components/Layout';
-import { SwitchBoardItem } from 'components/global/SwitchBoardItem';
+import Tiles from 'components/global/Tiles';
 
 export default function index() {
   const links = [
@@ -7,30 +7,29 @@ export default function index() {
       title: 'Company Information',
       route: '/company-information',
       color: 'bg-primary-pink',
-      icon: null,
+      textClass: 'whitespace-pre-wrap',
     },
     {
       title: 'Users',
       route: '/users',
       color: 'bg-primary-pink',
-      icon: null,
     },
     {
       title: 'Payment Details',
       route: '/payment-details',
       color: 'bg-primary-pink',
-      icon: null,
+      textClass: 'whitespace-pre-wrap w-[60px]',
     },
     {
       title: 'Account Preferences',
       route: '/account-preferences',
       color: 'bg-primary-pink',
-      icon: null,
+      textClass: 'whitespace-pre-wrap w-[80px]',
     },
   ];
   return (
     <Layout title="System Admin | Segue" flush>
-      <div className=" flex flex-col justify-center items-center h-full">
+      <div className=" flex flex-col justify-center items-center h-full  ">
         <h1 className="text-4xl font-bold  text-primary-pink">System Admin</h1>
         <ul
           data-testid="system-admin-tiles"
@@ -38,7 +37,7 @@ export default function index() {
           className="grid grid-cols-1 gap-4 w-fit sm:grid-cols-2 md:grid-cols-4 mt-20 mx-auto"
         >
           {links.map((link) => (
-            <SwitchBoardItem key={link.route} link={link} />
+            <Tiles key={link.route} link={link} />
           ))}
         </ul>
       </div>
