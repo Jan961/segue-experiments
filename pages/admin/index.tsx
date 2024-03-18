4902ef110f3699043055dc749668bbf8ce4ca4eb
Huzaifa-31 <31huzafasameer@gmail.com>
2024-03-18 17:33:21 +0500

    using SwitchBoardItem

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,32 +1,54 @@
 import Layout from 'components/Layout';
+import { SwitchBoardItem } from 'components/global/SwitchBoardItem';
 
 export default function index() {
+  const links = [
+    {
+      title: 'Company Information',
+      route: '/company-information',
+      icon: null,
+      stroke: '',
+      fill: '',
+      color: 'bg-primary-pink',
+    },
+    {
+      title: 'Users',
+      route: '/users',
+      icon: null,
+      stroke: '',
+      fill: '',
+      color: 'bg-primary-pink',
+    },
+    {
+      title: 'Payment Details',
+      route: '/payment-details',
+      icon: null,
+      stroke: '',
+      fill: '',
+      color: 'bg-primary-pink',
+    },
+    {
+      title: 'Account Preferences',
+      route: '/account-preferences',
+      icon: null,
+      stroke: '',
+      fill: '',
+      color: 'bg-primary-pink',
+    },
+  ];
   return (
-    <Layout title="System Admin" flush>
+    <Layout title="System Admin | Segue" flush>
       <div className=" flex flex-col justify-center items-center h-full">
         <h1 className="text-4xl font-bold  text-primary-pink">System Admin</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 pt-20">
-          <div className="bg-primary-pink w-[180px] h-[95px] flex justify-center items-center px-5 font-bold rounded-md shadow-md ">
-            <p className="text-white text-center">
-              Company <br /> Information
-            </p>
-          </div>
-          <div className="bg-primary-pink w-[180px] h-[95px] flex justify-center items-center px-5 font-bold rounded-md shadow-md">
-            <p className="text-white text-center">Users</p>
-          </div>
-          <div className="bg-primary-pink w-[180px] h-[95px] flex justify-center items-center px-5 font-bold rounded-md shadow-md">
-            <p className="text-white text-center">
-              Payment
-              <br /> Details
-            </p>
-          </div>
-          <div className="bg-primary-pink w-[180px] h-[95px] flex justify-center items-center px-5 font-bold rounded-md shadow-md">
-            <p className="text-white text-center">
-              Account <br />
-              Preferences
-            </p>
-          </div>
-        </div>
+        <ul
+          data-testid="system-admin-tiles"
+          role="list"
+          className="grid grid-cols-1 gap-4 w-fit sm:grid-cols-2 md:grid-cols-4 mt-20 mx-auto"
+        >
+          {links.map((link) => (
+            <SwitchBoardItem key={link.route} link={link} />
+          ))}
+        </ul>
       </div>
     </Layout>
   );
