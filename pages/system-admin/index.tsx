2eb5ab5cd3f182ecada58d33eb88bc329f2431de
Huzaifa-31 <31huzafasameer@gmail.com>
2024-03-18 14:58:34 +0500

    design done

---
+++ pages/system-admin/index.tsx
@@ -0,0 +1,25 @@
+import Layout from 'components/Layout';
+
+export default function index() {
+  return (
+    <Layout title="System Admin" flush>
+      <div className=" flex flex-col justify-center items-center h-full">
+        <h1 className="text-4xl font-bold  text-primary-pink">System Admin</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 pt-20">
+          <div className="bg-primary-pink py-10 px-5 font-bold rounded-md shadow-md ">
+            <p className="text-white text-center">Company Information</p>
+          </div>
+          <div className="bg-primary-pink py-10 px-5 font-bold rounded-md shadow-md">
+            <p className="text-white text-center">Users</p>
+          </div>
+          <div className="bg-primary-pink py-10 px-5 font-bold rounded-md shadow-md">
+            <p className="text-white text-center">Payment Details</p>
+          </div>
+          <div className="bg-primary-pink py-10 px-5 font-bold rounded-md shadow-md">
+            <p className="text-white text-center">Account Preferences</p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
