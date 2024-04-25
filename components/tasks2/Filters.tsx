ec235f9a6c4421d27ea6dd4440286e5956a34af6
Hruday kumar reddy <hrudaykumar11@gmail.com>
2024-04-25 13:18:12 +0530

    feat: initial commit for tasks

---
+++ components/tasks2/Filters.tsx
@@ -0,0 +1,99 @@
+import Button from 'components/core-ui-lib/Button';
+import GlobalToolbar from 'components/toolbar';
+import Report from 'components/bookings/modal/Report';
+import Select from 'components/core-ui-lib/Select';
+import TextInput from 'components/core-ui-lib/TextInput';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { filterState, intialBookingFilterState } from 'state/booking/filterState';
+import { useState } from 'react';
+import { allStatusOptions } from 'config/bookings';
+import { productionJumpState } from 'state/booking/productionJumpState';
+import useMileageCalculator from 'hooks/useBookingMileageCalculator';
+import TasksButtons from './TasksButtons';
+
+const Filters = () => {
+  const [filter, setFilter] = useRecoilState(filterState);
+  const { selected: ProductionId } = useRecoilValue(productionJumpState);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { loading: isMileageLoading } = useMileageCalculator();
+  const [showProductionSummary, setShowProductionSummary] = useState(false);
+  // const todayOnSchedule = useMemo(
+  //   () =>
+  //     schedule.Sections.map((x) => x.Dates)
+  //       .flat()
+  //       .filter((x) => x.Date === todayKey).length > 0,
+  //   [schedule.Sections, todayKey],
+  // );
+
+  const onChange = (e: any) => {
+    setFilter({ ...filter, [e.target.id]: e.target.value });
+  };
+  // const gotoToday = () => {
+  //   const dateToScrollTo = moment(new Date()).format('ddd DD/MM/YY');
+  //   if (todayOnSchedule) {
+  //     setFilter({ ...filter, scrollToDate: dateToScrollTo });
+  //   }
+  // };
+
+  const onClearFilters = () => {
+    setFilter({
+      ...intialBookingFilterState,
+      startDate: filter.scheduleStartDate,
+      endDate: filter.scheduleEndDate,
+      scheduleStartDate: filter.scheduleStartDate,
+      scheduleEndDate: filter.scheduleEndDate,
+    });
+  };
+
+  return (
+    <div className="w-full flex items-center justify-between flex-wrap">
+      <div className="mx-0">
+        <div className="px-4">
+          <GlobalToolbar
+            searchFilter={filter.venueText}
+            setSearchFilter={(venueText) => setFilter({ venueText })}
+            titleClassName="text-primary-yellow"
+            title={'Production Task Lists'}
+          >
+            <div className="flex items-center gap-4">
+              {showProductionSummary && (
+                <Report
+                  visible={showProductionSummary}
+                  onClose={() => setShowProductionSummary(false)}
+                  ProductionId={ProductionId}
+                />
+              )}
+            </div>
+          </GlobalToolbar>
+        </div>
+        <div className="px-4 flex items-center gap-4 flex-wrap  py-1">
+          <Select
+            onChange={(value) => onChange({ target: { id: 'status', value } })}
+            disabled={!ProductionId}
+            value={filter.status}
+            className="bg-white w-52"
+            label="Status"
+            options={allStatusOptions}
+          />
+        </div>
+        <div className="px-4 flex items-center gap-4 flex-wrap  py-1 mt-2">
+          <TextInput
+            id={'venueText'}
+            disabled={!ProductionId}
+            placeholder="Search bookings..."
+            className="w-[310px]"
+            iconName="search"
+            value={filter.venueText}
+            onChange={onChange}
+          />
+          <Button className="text-sm leading-8 w-[120px]" text="Clear Filters" onClick={onClearFilters} />
+        </div>
+      </div>
+      <div className="px-4">
+        <TasksButtons />
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -1,101 +1,51 @@
-import Layout from 'components/Layout';
-import Toolbar from 'components/tasks/toolbar';
-import Tasklist from 'components/tasks/TaskList';
-import GlobalToolbar from 'components/toolbar';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { getProductionsAndTasks } from 'services/productionService';
-import { ProductionsWithTasks, productionState } from 'state/tasks/productionState';
+import Layout from 'components/Layout';
 import { InitialState } from 'lib/recoil';
-import { mapToProductionTaskDTO } from 'lib/mappers';
-import { getAccountIdFromReq, getUsers } from 'services/userService';
-import { objectify } from 'radash';
 import { getProductionJumpState } from 'utils/getProductionJumpState';
-import useTasksFilter from 'hooks/useTasksFilter';
-import { useRecoilState } from 'recoil';
-import { ProductionTaskDTO } from 'interfaces';
+import { getAccountIdFromReq } from 'services/userService';
+import useBookingFilter from 'hooks/useBookingsFilter';
+import BookingsTable from 'components/bookings/BookingsTable';
+import Filters from 'components/tasks2/Filters';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Index = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { filteredProductions, onApplyFilters } = useTasksFilter();
-  const [productionTasks, setProductionTasks] = useRecoilState(productionState);
-  const onTasksChange = (updatedTasks: ProductionTaskDTO[], productionId: number) => {
-    const updatedProductionTasks = productionTasks.map((productionTask) => {
-      if (productionTask.Id === productionId) {
-        return { ...productionTask, Tasks: updatedTasks };
-      }
-      return productionTask;
-    });
-    setProductionTasks(updatedProductionTasks);
-  };
+const TasksPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const rows = useBookingFilter();
+
   return (
-    <Layout title="Tasks | Segue">
-      <div className="flex flex-auto w-full h-screen">
-        <div className="flex-col px-12 w-full flex" style={{ minHeight: '60vh' }}>
-          <GlobalToolbar productionJump={false} title={'Tasks'} color={'!text-purple-900'} />
-          <Toolbar onApplyFilters={onApplyFilters} />
-          {filteredProductions.length > 0 ? (
-            filteredProductions.map((production) => {
-              return (
-                <div key={production.Id} className="mb-10">
-                  <h3 className=" text-xl font-bold py-4 !text-purple-900">{production.ShowName}</h3>
-                  <Tasklist
-                    onTasksChange={(change) => onTasksChange(change, production.Id)}
-                    tasks={production?.Tasks}
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <div className="text-center font-bold text-lg">
-              <p>No Tasks Found</p>
-            </div>
-          )}
-          {/* <TaskButtons openBulkModal={openBulkModal} /> */}
-        </div>
+    <Layout title="Tasks | Segue" flush>
+      <div className="mb-8">
+        <Filters />
       </div>
-      {/*
-        <BulkActionForm
-          userAccountId={0}
-          closeModal={console.log}
-          taskIdArray={isSelectedArray}
-          bulkActionField={bulkActionField}
-        />
-        */}
+      <BookingsTable rowData={rows} />
     </Layout>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const AccountId = await getAccountIdFromReq(ctx.req);
-  const productionJump = await getProductionJumpState(ctx, 'tasks', AccountId);
-  const productionsWithTasks = await getProductionsAndTasks(AccountId);
-  const users = await getUsers(AccountId);
+export default TasksPage;
 
-  const productions: ProductionsWithTasks[] = productionsWithTasks.map((t: any) => ({
-    Id: t.Id,
-    ShowName: t.Show.Name,
-    ShowCode: t.Show.Code,
-    ShowId: t.Show.Id,
-    Code: t.Code,
-    Tasks: t.ProductionTask.map(mapToProductionTaskDTO)
-      .map((task) => ({
-        ...task,
-        StartDate: t.WeekNumToDateMap[task.StartByWeekNum],
-        CompleteDate: t.WeekNumToDateMap[task.CompleteByWeekNum],
-      }))
-      .sort((a, b) => a.StartByWeekNum - b.StartByWeekNum),
-    weekNumToDateMap: t.WeekNumToDateMap,
-  }));
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  /*
+    We get the data for the whole booking page here. We pass it to the constructor, then store it in state management.
+    This means we can update a single booking, and the schedule will update without having to redownload all the data.
+    We have effectively cloned the database for this production, and populate it using the results of a single query which includes 'everything we want
+    to display'.
+
+    The itinery or miles will be different however, as this relies on the preview booking, and has to be generateed programatically
+  */
+  const AccountId = await getAccountIdFromReq(ctx.req);
+  const productionJump = await getProductionJumpState(ctx, 'bookings', AccountId);
+  const ProductionId = productionJump.selected;
+  // See _app.tsx for how this is picked up
   const initialState: InitialState = {
     global: {
       productionJump,
     },
-    tasks: { productions, bulkSelection: {} },
-    account: {
-      user: { users: objectify(users, (user) => user.Id) },
+  };
+
+  return {
+    props: {
+      ProductionId,
+      initialState,
     },
   };
-  return { props: { initialState } };
 };
-
-export default Index;
