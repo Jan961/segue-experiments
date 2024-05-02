a42d062e39f5630b49d8195772e8ba2e2bb0241b
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-05-02 17:43:57 +0100

    removed tab specific logic in the MarketingHome.tsx and added it to their own files

--- components/marketing/tabs/ArchivedSalesTab.tsx
+++ components/marketing/tabs/ArchivedSalesTab.tsx
@@ -1,130 +1,112 @@
-import { Table } from 'components/global/table/Table';
-
-import BookingSelection from './BookingSelection';
-import React, { useEffect, useMemo } from 'react';
-import axios from 'axios';
-import { useRecoilValue } from 'recoil';
+import Button from 'components/core-ui-lib/Button';
+import { ReactNode, useState } from 'react';
+import ArchSalesDialog, { ArchSalesDialogVariant } from '../modal/ArchivedSalesDialog';
+import { DataList, VenueDetail } from '../MarketingHome';
+import SalesTable from 'components/global/salesTable';
+import { SalesComparison } from 'types/MarketingTypes';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import useAxios from 'hooks/useAxios';
+import { townState } from 'state/marketing/townState';
+import { venueState } from 'state/booking/venueState';
 import { bookingJumpState } from 'state/marketing/bookingJumpState';
-import { LoadingTab } from './LoadingTab';
-import { useRouter } from 'next/router';
-import { dateToSimple } from 'services/dateService';
-import { productionJumpState } from 'state/booking/productionJumpState';
-import classNames from 'classnames';
 
 export const ArchivedSalesTab = () => {
-  const router = useRouter();
-  const [archivedSales, setArchivedSales] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
-  const [salesByType, setSalesByType] = React.useState(null);
-  const { selected, bookings } = useRecoilValue(bookingJumpState);
-  const { selected: selectedProduction, productions: productionsList } = useRecoilValue(productionJumpState);
-  const [productions, setProductions] = React.useState<any[]>([]);
-  const selectedBooking = useMemo(() => bookings.find((booking) => booking.Id === selected), [bookings, selected]);
-  const tableWidth = useMemo(() => 260 + productions.length * 180, [productions]);
-  const openBookingSelection = (type) => {
-    setSalesByType(type);
+  const [showArchSalesModal, setShowArchSalesModal] = useState<boolean>(false);
+  const [archSaleVariant, setArchSaleVariant] = useState<ArchSalesDialogVariant>('venue');
+  const [archivedDataAvail, setArchivedDataAvail] = useState<boolean>(false);
+  const [archivedData, setArchivedData] = useState<VenueDetail | DataList>();
+  const [archivedSalesTable, setArchivedSalesTable] = useState<ReactNode>();
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const townList = useRecoilValue(townState);
+  const venueDict = useRecoilValue(venueState);
+  const bookings = useRecoilState(bookingJumpState);
+
+  const { fetchData } = useAxios();
+
+  const showArchSalesComp = (variant: ArchSalesDialogVariant) => {
+    setArchSaleVariant(variant);
+    if (variant === 'both') {
+      // get venue list
+      const venueTownData = {
+        townList: Object.values(townList).map((town) => {
+          return { text: town.Town, value: town.Town };
+        }),
+        venueList: Object.values(venueDict).map((venue) => {
+          return { text: venue.Code + ' ' + venue.Name, value: venue };
+        }),
+      };
+      setArchivedData(venueTownData);
+    } else {
+      const selectedBooking = bookings[0].bookings.find((booking) => booking.Id === bookings[0].selected);
+      // extract the venue name, code and town
+      const venue = {
+        name: selectedBooking.Venue.Name,
+        code: selectedBooking.Venue.Code,
+        town: Object.values(venueDict).find((x) => x.Code === selectedBooking.Venue.Code).Town,
+      };
+
+      setArchivedData(venue);
+    }
+
+    setShowArchSalesModal(true);
   };
-  const getBookingSales = async (bookingIds, productions) => {
-    setProductions(productions);
-    setLoading(true);
-    try {
-      const { data } = await axios.post('/api/marketing/sales/read/archived', {
-        bookingIds,
-      });
-      setArchivedSales(data.response);
-    } catch (error: any) {
-      console.error(error);
-    } finally {
-      setLoading(false);
+
+  const showArchivedSales = async (selection) => {
+    setArchivedSalesTable(<div />);
+    const selectedBookings = selection.map((obj) => obj.bookingId);
+    const data = await fetchData({
+      url: '/api/marketing/sales/read/archived',
+      method: 'POST',
+      data: { bookingIds: selectedBookings },
+    });
+
+    if (Array.isArray(data) && data.length !== 0) {
+      const salesComp = data as Array<SalesComparison>;
+      const result = { tableData: salesComp, bookingIds: selection };
+
+      setArchivedSalesTable(
+        <SalesTable
+          containerHeight="h-[1000px]"
+          containerWidth="w-auto"
+          module="marketing"
+          variant="salesComparison"
+          data={result}
+          tableHeight={560}
+        />,
+      );
+      setArchivedDataAvail(true);
+      setShowArchSalesModal(false);
+    } else {
+      setErrorMessage('There are no sales data available for this particular selection.');
     }
   };
-  useEffect(() => {
-    if (selected) {
-      const production = productionsList.find((production) => production.Id === selectedProduction);
-      getBookingSales([selected], [{ FullProductionCode: `${production.ShowCode}${production.Code}` }]);
-    }
-  }, [selected]);
-  if (loading) return <LoadingTab />;
 
   return (
     <>
-      <div className={'mb-1 space-x-3 pl-1 pb-4 mt-4'}>
-        <button
-          onClick={() => openBookingSelection('venue')}
-          className={
-            'inline-flex items-center rounded-md  bg-white  px-6 py-3 text-xs font-bold drop-shadow-lg text-primary-green hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-          }
-        >
-          For this Venue{' '}
-        </button>
-        <button
-          onClick={() => openBookingSelection('town')}
-          className={
-            'inline-flex items-center rounded-md  bg-white  px-6 py-3 text-xs font-bold drop-shadow-lg text-primary-green hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-          }
-        >
-          For this Town{' '}
-        </button>
-        {salesByType && (
-          <BookingSelection
-            venueCode={selectedBooking?.Venue?.Code}
-            showCode={router.query.ShowCode}
-            salesByType={salesByType}
-            onSubmit={getBookingSales}
-            selectedBookingId={selectedBooking?.Id}
-            onClose={() => setSalesByType(null)}
+      <div>
+        <div className="flex flex-row gap-4 mb-5">
+          <Button text="For this Venue" className="w-[132px]" onClick={() => showArchSalesComp('venue')} />
+          <Button text="For this Town" className="w-[132px]" onClick={() => showArchSalesComp('town')} />
+          <Button text="Any Venue / Town" className="w-[132px]" onClick={() => showArchSalesComp('both')} />
+          <Button
+            text="Export Displayed Sales Data"
+            className="w-[232px]"
+            iconProps={{ className: 'h-4 w-3 ml-5' }}
+            sufixIconName={'excel'}
+            disabled={!archivedDataAvail}
           />
-        )}
+
+          <ArchSalesDialog
+            show={showArchSalesModal}
+            variant={archSaleVariant}
+            data={archivedData}
+            onCancel={() => setShowArchSalesModal(false)}
+            onSubmit={(bookings) => showArchivedSales(bookings)}
+            error={errorMessage}
+          />
+        </div>
+        {archivedSalesTable}
       </div>
-      {archivedSales?.length ? (
-        <Table className={'table-auto !min-w-0 sticky-header-table'} style={{ maxWidth: tableWidth }}>
-          <Table.HeaderRow className="rounded-t-lg">
-            <Table.HeaderCell className="rounded-tl-lg"></Table.HeaderCell>
-            {productions.map((production, i) => (
-              <>
-                <Table.HeaderCell
-                  className={classNames('text-center !text-lg', { 'rounded-tr-lg': i === productions.length - 1 })}
-                  key={i}
-                >
-                  {production.FullProductionCode}
-                </Table.HeaderCell>
-              </>
-            ))}
-          </Table.HeaderRow>
-          <Table.HeaderRow>
-            <Table.HeaderCell className="text-center">Week</Table.HeaderCell>
-            <Table.HeaderCell className="text-center">Week of</Table.HeaderCell>
-            {productions.map((_) => (
-              <>
-                <Table.HeaderCell className={classNames('text-center')}>Seats</Table.HeaderCell>
-                <Table.HeaderCell className={classNames('text-center')}>Value</Table.HeaderCell>
-              </>
-            ))}
-          </Table.HeaderRow>
-          <Table.Body className="" style={{ maxHeight: 'calc(100vh - 500px)' }}>
-            {archivedSales.map((sale, i) => (
-              <Table.Row key={i}>
-                <Table.Cell className="text-center">
-                  {sale.SetIsFinalFigures ? 'Final' : `Week ${sale.SetBookingWeekNum}`}
-                </Table.Cell>
-                <Table.Cell>{dateToSimple(sale.SetProductionWeekDate)}</Table.Cell>
-                {sale.data.map((productionSale, i) => (
-                  <>
-                    <Table.Cell className={classNames('text-right', { 'bg-gray-100': i % 2 === 0 })}>
-                      {productionSale.Seats}
-                    </Table.Cell>
-                    <Table.Cell className={classNames('text-right', { 'bg-gray-100': i % 2 === 0 })}>
-                      {productionSale.ValueWithCurrencySymbol}
-                    </Table.Cell>
-                  </>
-                ))}
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      ) : (
-        <div>Please select archived productions</div>
-      )}
     </>
   );
 };
