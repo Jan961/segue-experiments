3f281a4cd1dcc75729acd9a93520907ac8e5fe95
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-05-15 17:13:15 +0100

    a number of the marketing bugs from SK-171 fixed

--- mappers/productionCodeMapper.ts
+++ mappers/productionCodeMapper.ts
@@ -1,4 +1,5 @@
-import { getWeekDay, formatDateUK } from 'services/dateService';
+import { getWeekDayShort } from 'services/dateService';
+import formatInputDate from 'utils/dateInputFormat';
 
 export const mapBookingsToProductionOptions = (bookings) => {
   if (!bookings || bookings.length === 0) {
@@ -8,10 +9,10 @@ export const mapBookingsToProductionOptions = (bookings) => {
     .filter((b) => b.StatusCode !== 'U')
     .map((b) => {
       const date = new Date(b.Date);
-      const weekday = getWeekDay(date);
-      const ukDate = formatDateUK(date);
+      const weekday = getWeekDayShort(date).toUpperCase();
+      const ukDate = formatInputDate(date);
       return {
-        text: `${b.Venue.Code} ${b.Venue.Name} ${weekday} ${ukDate} (${b.StatusCode})`,
+        text: `${b.Venue.Code} ${b.Venue.Name} | ${weekday} ${ukDate} (${b.StatusCode})`,
         value: `${b.Id}`,
         date: ukDate,
       };
