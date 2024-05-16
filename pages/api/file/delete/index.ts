222caedb1198307edc7555111fbbb6c5971f6c74
Arun <asara97911@gmail.com>
2024-05-16 09:02:46 +0100

    changed api

--- pages/api/file/delete/index.ts
+++ pages/api/file/delete/index.ts
@@ -7,20 +7,20 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
   if (req.method !== 'DELETE') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
-  const { id } = req.query;
-  if (!id || Array.isArray(id)) {
+  const { location: Location } = req.body;
+  if (!location || Array.isArray(location)) {
     return res.status(400).json({ message: 'Invalid request' });
   }
   try {
     const file: File = await prisma.File.findUnique({
-      where: { Id: Number(id) },
+      where: { Location },
     });
     if (!file) {
       return res.status(404).json({ message: 'File not found' });
     }
     await deleteFile(file.Location);
     const deletedFile = await prisma.File.delete({
-      where: { Id: Number(id) },
+      where: { Location },
     });
     res.status(200).json(deletedFile);
   } catch (error) {
