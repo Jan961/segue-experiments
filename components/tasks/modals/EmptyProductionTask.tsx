3659ee8eea6e4c4149c90444e3d93ccc3f962789
Hruday kumar reddy <hrudaykumar11@gmail.com>
2024-05-24 13:30:54 +0530

    feat: removed unused loader

--- components/tasks/modals/EmptyProductionTask.tsx
+++ components/tasks/modals/EmptyProductionTask.tsx
@@ -1,15 +1,8 @@
 import Button from 'components/core-ui-lib/Button';
-import Loader from 'components/core-ui-lib/Loader';
 import PopupModal from 'components/core-ui-lib/PopupModal';
 import Select from 'components/core-ui-lib/Select';
 import { useState } from 'react';
 import { NewProductionTaskOptions } from './constants';
-
-const LoadingOverlay = () => (
-  <div className="inset-0 absolute bg-white bg-opacity-50 z-50 flex justify-center items-center">
-    <Loader className="ml-2" iconProps={{ stroke: '#FFF' }} />
-  </div>
-);
 
 interface EmptyProductionTaskProps {
   visible: boolean;
@@ -17,16 +10,10 @@ interface EmptyProductionTaskProps {
 }
 
 const EmptyProductionTask = ({ visible, onClose }: EmptyProductionTaskProps) => {
-  const [loading, setLoading] = useState<boolean>(false);
-
   const [newProductionTask, setNewProductionTask] = useState('master');
 
   const handleOnChange = (value: string) => {
     setNewProductionTask(value);
-  };
-
-  const handleOnSubmit = async () => {
-    setLoading(true);
   };
 
   const handleClose = () => {
@@ -41,7 +28,6 @@ const EmptyProductionTask = ({ visible, onClose }: EmptyProductionTaskProps) => 
       titleClass="text-primary-navy text-xl mb-4"
     >
       <form className="flex flex-col gap-4">
-        {loading && <LoadingOverlay />}
         <p>
           The selected production has no tasks.
           <br />
@@ -57,7 +43,7 @@ const EmptyProductionTask = ({ visible, onClose }: EmptyProductionTaskProps) => 
         />
         <div className="flex">
           <Button variant="secondary" onClick={onClose} className="mr-4 w-[132px]" text="Cancel" />
-          <Button variant="primary" className="w-[132px]" onClick={handleOnSubmit} text={'Ok'} />
+          <Button variant="primary" className="w-[132px]" onClick={null} text={'Ok'} />
         </div>
       </form>
     </PopupModal>
