5692643d299716ceaa345405cae57706dbff9e40
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-05-29 10:33:13 +0100

    revered api change and added create multiple endpoint

--- pages/api/tasks/master/create.ts
+++ pages/api/tasks/master/create.ts
@@ -6,28 +6,16 @@ import { getAccountId, getEmailFromReq } from 'services/userService';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const tasks = req.body as MasterTask[];
+    const task = req.body as MasterTask;
     const email = await getEmailFromReq(req);
     const AccountId = await getAccountId(email);
-
-    // Get the maximum task code once
     const { Code } = await getMaxTaskCode();
-
-    // Map the tasks to include the AccountId and incremented Code
-    const tasksWithAccountAndCode = tasks.map((task, index) => ({
-      ...task,
-      AccountId,
-      Code: Code + 1 + index, // Increment code for each task
-    }));
-
-    // Use createMany to insert all tasks at once
-    const createResults = await prisma.MasterTask.createMany({
-      data: tasksWithAccountAndCode,
+    const createResult = await prisma.MasterTask.create({
+      data: { ...task, AccountId, Code: Code + 1 },
     });
-
-    res.json(createResults);
+    res.json(createResult);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: 'Error creating Master Tasks' });
+    res.status(500).json({ error: 'Error creating Master Task' });
   }
 }
