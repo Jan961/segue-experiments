a5dcf5e20351f5f0d1d1727bd924e6ae1e95b7ce
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-05-31 16:00:15 +0100

    Removed console.log

--- components/bookings/modal/NewBooking/index.tsx
+++ components/bookings/modal/NewBooking/index.tsx
@@ -110,7 +110,6 @@ const AddBooking = ({ visible, onClose, startDate, endDate, booking }: AddBookin
       const runOfDates = bookings
         .filter(({ runTag }) => runTag === booking.runTag)
         .sort((a, b) => new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime());
-      console.log('bookings', runOfDates, bookings);
       if (runOfDates.length > 1) {
         onFormDataChange({ isRunOfDates: true });
       }
