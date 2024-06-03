e61468b4ee2519d580c07416b2c679cef0b0d667
David Kelly <davidrkelly07@gmail.com>
2024-06-03 10:26:37 +0100

    Fixed the broken filter.

--- state/booking/selectors/venueOptionsSelector.ts
+++ state/booking/selectors/venueOptionsSelector.ts
@@ -1,22 +1,29 @@
 import { VenueMinimalDTO } from 'interfaces';
 import { selectorFamily } from 'recoil';
 import { venueState } from 'state/booking/venueState';
+import { currentProductionSelector } from './currentProductionSelector';
 
 export const venueOptionsSelector = selectorFamily({
   key: 'venueOptionsSelector',
   get:
     (excludedVenueIds: number[] = []) =>
     ({ get }) => {
+      const currentProduction = get(currentProductionSelector);
       const venueDict: Record<number, VenueMinimalDTO> = get(venueState);
       const options = [];
-
       for (const venue of Object.values(venueDict)) {
-        if (excludedVenueIds.includes(venue.Id)) continue;
-        options.push({
-          text: `${venue.Code} ${venue?.Name} ${venue?.Town}`,
-          value: venue?.Id,
-        });
+        // Pushes options without regions
+        if (excludedVenueIds.includes(venue.Id) || venue.RegionId !== currentProduction.ShowRegionId) continue;
+
+        if (venue.RegionId === -1 || venue.RegionId === currentProduction.ShowRegionId) {
+          options.push({
+            text: `${venue.Code} ${venue?.Name} ${venue?.Town}`,
+
+            value: venue?.Id,
+          });
+        }
       }
+
       return options.sort((a, b) => a.text.localeCompare(b.text));
     },
 });
