c11aac635ceb9e3dd3b3e4b877b76d024bc160d1
David Kelly <davidrkelly07@gmail.com>
2024-06-06 16:56:34 +0100

    Filtering for master task list

--- hooks/useMasterTaskFilter.ts
+++ hooks/useMasterTaskFilter.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
-import { getFilteredUsers } from './useTasksFilter';
 import { useRecoilValue } from 'recoil';
 import { masterTaskState } from 'state/tasks/masterTaskState';
 import { userState } from 'state/account/userState';
+import Fuse from 'fuse.js';
 
 const useMasterTasksFilter = (tasks = []) => {
   const filters = useRecoilValue(masterTaskState);
@@ -14,19 +14,39 @@ const useMasterTasksFilter = (tasks = []) => {
       text: `${FirstName || ''} ${LastName || ''}`,
     }));
   }, [users]);
+  const userIdToNameMap = usersList.reduce((acc, user) => {
+    acc[user.value] = user.text;
+    return acc;
+  }, {});
+
+  const fuseOptions = {
+    includeScore: true,
+    includeMatches: true,
+    isCaseSensitive: false,
+    shouldSort: true,
+    useExtendedSearch: true,
+    threshold: 0.3,
+    keys: [],
+    // keys: ['Name', 'Notes'],
+  };
 
   const filteredTasks = useMemo(() => {
-    return tasks.filter(({ Name, TaskName, AssignedToUserId, Notes }) => {
-      return (
-        !filters.taskText ||
-        [Name, TaskName, Notes]
-          .map((text) => text?.toLowerCase?.())
-          .some((text) => text?.includes?.(filters.taskText.toLowerCase())) ||
-        getFilteredUsers(usersList, AssignedToUserId, filters.taskText)
-      );
-    });
-  }, [tasks, filters.taskText, usersList]);
-
+    fuseOptions.keys = ['Name', 'userName'];
+    return filters.taskText
+      ? new Fuse(
+          tasks.map((task) => {
+            return {
+              ...task,
+              userName: task.AssignedToUserId !== -1 ? userIdToNameMap[task.AssignedToUserId] : null,
+            };
+          }),
+          fuseOptions,
+        )
+          .search(filters.taskText)
+          .map((item) => item.item)
+      : tasks;
+  }, [tasks, filters.taskText]);
+  console.log(filteredTasks);
   return { filteredTasks };
 };
 
