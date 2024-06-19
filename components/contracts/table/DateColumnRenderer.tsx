64af5e20b3e368b33280a0ae822f98de35eb48cb
David Kelly <davidrkelly07@gmail.com>
2024-06-19 16:23:42 +0100

    Using tailwind in the DateRenderer

--- components/contracts/table/DateColumnRenderer.tsx
+++ components/contracts/table/DateColumnRenderer.tsx
@@ -4,7 +4,7 @@ export default function DateColumnRenderer(props: CustomCellRendererProps) {
   const isMonday = props.value ? props.value.includes('Mon') : false;
   return (
     <div className="h-full pr-[2px]">
-      <div className={`px-1 truncate ${isMonday ? 'bg-[#FDCE74]' : ''}`}>{props.value || ''}</div>
+      <div className={`px-1 truncate ${isMonday ? 'bg-primary-yellow' : ''}`}>{props.value || ''}</div>
     </div>
   );
 }
