bb0da0a38916a1bd252c5cf12101cd95281d7569
Arun <asara97911@gmail.com>
2024-07-16 00:37:31 +0100

    allocated seats report

--- pages/api/reports/marketing/contact-notes/[bookingId].ts
+++ pages/api/reports/marketing/contact-notes/[bookingId].ts
@@ -1,47 +1,10 @@
-import { Prisma } from '@prisma/client';
 import ExcelJS from 'exceljs';
 import moment from 'moment';
-import prisma from 'lib/prisma';
 import { getContactNotesByBookingId } from 'services/venueContactsService';
 import { bookingContactNoteMapper } from 'lib/mappers';
 import { COLOR_HEXCODE } from 'services/salesSummaryService';
 import { NextApiRequest, NextApiResponse } from 'next';
-
-const getProductionAndVenueDetailsFromBookingId = async (bookingId: number) => {
-  const conditions: Prisma.Sql[] = [Prisma.sql` EntryType=${'Booking'}`];
-  if (bookingId) {
-    conditions.push(Prisma.sql` EntryId=${bookingId}`);
-  }
-  const where: Prisma.Sql = conditions.length ? Prisma.sql` where ${Prisma.join(conditions, ' and ')}` : Prisma.empty;
-  const results: any[] =
-    await prisma.$queryRaw`SELECT FullProductionCode, ShowName, EntryName FROM ScheduleView ${where}`;
-  if (results.length) {
-    const { FullProductionCode, ShowName, EntryName } = results[0];
-    return {
-      prodCode: FullProductionCode,
-      showName: ShowName,
-      venueName: EntryName,
-    };
-  }
-  return {};
-};
-
-const createHeaderRow = (worksheet: any, text: string, size: number) => {
-  const row = worksheet.addRow([text]);
-  row.height = 30;
-  const cell = row.getCell(1);
-  cell.font = { bold: true, size, color: { argb: COLOR_HEXCODE.WHITE } };
-  cell.fill = {
-    type: 'pattern',
-    pattern: 'solid',
-    fgColor: { argb: '41A29A' },
-  };
-  cell.alignment = { vertical: 'middle', horizontal: 'center' };
-  cell.border = {
-    bottom: { style: 'thin', color: { argb: COLOR_HEXCODE.WHITE } },
-  };
-  worksheet.mergeCells(`A${row.number}:E${row.number}`);
-};
+import { createHeaderRow, getProductionAndVenueDetailsFromBookingId } from 'services/marketing/reports';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { bookingId } = req.query || {};
