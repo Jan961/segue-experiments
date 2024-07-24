a5c1aefa86bffe04c6245c3a6b6d0bf0e94aaa79
guru prasad <guruprasad8212@gmail.com>
2024-07-24 16:28:49 +0530

    dev

---
+++ prisma/views/frtxigoo_dev/ShowView.sql
@@ -0,0 +1,9 @@
+SELECT
+  `frtxigoo_dev`.`Show`.`ShowId` AS `ShowId`,
+  `frtxigoo_dev`.`Show`.`ShowCode` AS `ShowCode`,
+  `frtxigoo_dev`.`Show`.`ShowName` AS `ShowName`,
+  `frtxigoo_dev`.`Show`.`ShowType` AS `ShowType`,
+  `frtxigoo_dev`.`Show`.`ShowIsArchived` AS `ShowIsArchived`,
+  `frtxigoo_dev`.`Show`.`ShowAccountId` AS `ShowAccountId`
+FROM
+  `frtxigoo_dev`.`Show`
---
+++ prisma/views/frtxigoo_dev/VenueView.sql
@@ -0,0 +1,76 @@
+SELECT
+  `frtxigoo_dev`.`Venue`.`VenueId` AS `VenueId`,
+  `frtxigoo_dev`.`Venue`.`VenueCode` AS `VenueCode`,
+  `frtxigoo_dev`.`Venue`.`VenueName` AS `VenueName`,
+  `frtxigoo_dev`.`Venue`.`VenueStatusCode` AS `VenueStatusCode`,
+  `frtxigoo_dev`.`Venue`.`VenueWebsite` AS `VenueWebsite`,
+  `frtxigoo_dev`.`VenueFamily`.`VenueFamilyName` AS `VenueFamily`,
+  `MainCountry`.`CountryCurrencyCode` AS `VenueCurrencyCode`,
+  `frtxigoo_dev`.`Venue`.`VenueVATIndicator` AS `VenueVATIndicator`,
+  `frtxigoo_dev`.`Venue`.`VenueTechSpecsURL` AS `VenueTechSpecsURL`,
+  `frtxigoo_dev`.`Venue`.`VenueSeats` AS `VenueSeats`,
+  `frtxigoo_dev`.`Venue`.`VenueBarringClause` AS `VenueBarringClause`,
+  `frtxigoo_dev`.`Venue`.`VenueTownPopulation` AS `VenueTownPopulation`,
+  `frtxigoo_dev`.`Venue`.`VenueLXDesk` AS `VenueLXDesk`,
+  `frtxigoo_dev`.`Venue`.`VenueLXNotes` AS `VenueLXNotes`,
+  `frtxigoo_dev`.`Venue`.`VenueSoundDesk` AS `VenueSoundDesk`,
+  `frtxigoo_dev`.`Venue`.`VenueSoundNotes` AS `VenueSoundNotes`,
+  `frtxigoo_dev`.`Venue`.`VenueStageSize` AS `VenueStageSize`,
+  `frtxigoo_dev`.`Venue`.`VenueGridHeight` AS `VenueGridHeight`,
+  `frtxigoo_dev`.`Venue`.`VenueVenueFlags` AS `VenueVenueFlags`,
+  `frtxigoo_dev`.`Venue`.`VenueBarringWeeksPre` AS `VenueBarringWeeksPre`,
+  `frtxigoo_dev`.`Venue`.`VenueBarringWeeksPost` AS `VenueBarringWeeksPost`,
+  `frtxigoo_dev`.`Venue`.`VenueBarringMiles` AS `VenueBarringMiles`,
+  `frtxigoo_dev`.`Venue`.`VenueCulturallyExempt` AS `VenueCulturallyExempt`,
+  `frtxigoo_dev`.`Venue`.`VenueAccountId` AS `VenueAccountId`,
+  `frtxigoo_dev`.`Venue`.`VenueIsDeleted` AS `VenueIsDeleted`,
+  `MainAddress`.`VenueAddress1` AS `VenueMainAddress1`,
+  `MainAddress`.`VenueAddress2` AS `VenueMainAddress2`,
+  `MainAddress`.`VenueAddress3` AS `VenueMainAddress3`,
+  `MainAddress`.`VenueAddressTown` AS `VenueMainAddressTown`,
+  `MainAddress`.`VenueAddressCounty` AS `VenueMainAddressCounty`,
+  `MainAddress`.`VenueAddressPostcode` AS `VenueMainAddressPostcode`,
+  `MainCountry`.`CountryName` AS `VenueMainAddressCountry`,
+  `DeliveryAddress`.`VenueAddress1` AS `VenueDeliveryAddress1`,
+  `DeliveryAddress`.`VenueAddress2` AS `VenueDeliveryAddress2`,
+  `DeliveryAddress`.`VenueAddress3` AS `VenueDeliveryAddress3`,
+  `DeliveryAddress`.`VenueAddressTown` AS `VenueDeliveryAddressTown`,
+  `DeliveryAddress`.`VenueAddressCounty` AS `VenueDeliveryAddressCounty`,
+  `DeliveryAddress`.`VenueAddressPostcode` AS `VenueDeliveryAddressPostcode`,
+  `DeliveryCountry`.`CountryName` AS `VenueDeliveryAddressCountry`,
+  `frtxigoo_dev`.`Venue`.`VenueNotes` AS `VenueMainNoteText`,
+  `frtxigoo_dev`.`Venue`.`VenueWarningNotes` AS `VenueWarningNoteText`
+FROM
+  (
+    (
+      (
+        (
+          (
+            (
+              `frtxigoo_dev`.`Venue`
+              LEFT JOIN `frtxigoo_dev`.`VenueAddress` `MainAddress` ON(
+                `frtxigoo_dev`.`Venue`.`VenueId` = `MainAddress`.`VenueAddressVenueId`
+                AND `MainAddress`.`VenueAddressTypeName` = 'Main'
+              )
+            )
+            LEFT JOIN `frtxigoo_dev`.`VenueAddress` `DeliveryAddress` ON(
+              `frtxigoo_dev`.`Venue`.`VenueId` = `DeliveryAddress`.`VenueAddressVenueId`
+              AND `DeliveryAddress`.`VenueAddressTypeName` = 'Delivery'
+            )
+          )
+          LEFT JOIN `frtxigoo_dev`.`VenueFamily` ON(
+            `frtxigoo_dev`.`Venue`.`VenueVenueFamilyId` = `frtxigoo_dev`.`VenueFamily`.`VenueFamilyId`
+          )
+        )
+        LEFT JOIN `frtxigoo_dev`.`Country` `MainCountry` ON(
+          `MainAddress`.`VenueAddressCountryId` = `MainCountry`.`CountryId`
+        )
+      )
+      LEFT JOIN `frtxigoo_dev`.`Currency` `MainCurrency` ON(
+        `MainCountry`.`CountryCurrencyCode` = `MainCurrency`.`CurrencyCode`
+      )
+    )
+    LEFT JOIN `frtxigoo_dev`.`Country` `DeliveryCountry` ON(
+      `DeliveryAddress`.`VenueAddressCountryId` = `DeliveryCountry`.`CountryId`
+    )
+  )
