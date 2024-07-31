ae04e187857a0beeb457b6fc5255ea69e00f17de
Matthew Waters <matthewwaters100@gmail.com>
2024-07-31 14:57:48 +0100

    Allowed JPEG file types

--- components/admin/AccountLogoUpload/AccountLogoUpload.tsx
+++ components/admin/AccountLogoUpload/AccountLogoUpload.tsx
@@ -105,7 +105,7 @@ export const AccountLogoUpload = () => {
             title="Upload Company Logo"
             visible={openUploadModal}
             info="Please upload your company logo here. Image should be no larger than 300px wide x 200px high (Max 500kb). Images in a square or portrait format will be proportionally scaled to fit with the rectangular boundary box. Suitable image formats are jpg, tiff, svg, and png."
-            allowedFormats={['image/jpg', 'image/tiff', 'image/svg', 'image/png']}
+            allowedFormats={['image/jpeg', 'image/tiff', 'image/svg+xml', 'image/png']}
             onClose={() => {
               setOpenUploadModal(false);
             }}
