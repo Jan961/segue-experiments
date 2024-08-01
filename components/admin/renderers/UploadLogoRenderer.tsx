c8be7b9450e65f3055a7f17c61a7979edb8d67c8
Matthew Waters <matthewwaters100@gmail.com>
2024-08-01 09:15:27 +0100

    Allowed for jpeg images in prod comp image upload

--- components/admin/renderers/UploadLogoRenderer.tsx
+++ components/admin/renderers/UploadLogoRenderer.tsx
@@ -74,7 +74,7 @@ export const UploadLogoRenderer = (params, fetchProductionCompanies, onUploadSuc
         title="Image Attachment"
         info="Please upload your company image here. Image should be no larger than 300px wide x 200px high (Max 500kb). Images in a square or portrait format will be proportionally scaled to fit with the rectangular boundary box.
 Suitable image formats are jpg, tiff, svg, and png."
-        allowedFormats={['image/jpg', 'image/png', 'image/tiff', 'image/svg']}
+        allowedFormats={['image/jpeg', 'image/png', 'image/tiff', 'image/svg']}
         onClose={() => {
           setOpenUploadModal(false);
           fetchProductionCompanies();
