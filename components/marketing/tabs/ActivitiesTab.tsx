4d30160ade7e2da8069970930c8f7d0d9b3900c5
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-08-02 15:48:41 +0100

    consists of changes to the currency of the global activities

--- components/marketing/tabs/ActivitiesTab.tsx
+++ components/marketing/tabs/ActivitiesTab.tsx
@@ -123,6 +123,7 @@ const ActivitiesTab = forwardRef<ActivityTabRef, ActivitiesTabProps>((props, ref
         }));
 
         calculateActivityTotals(tempRows);
+
         setActRowData(tempRows);
       }
 
@@ -563,7 +564,7 @@ const ActivitiesTab = forwardRef<ActivityTabRef, ActivitiesTabProps>((props, ref
           <div
             className={classNames(
               'flex flex-col w-[487px] h-[69px] bg-primary-green/[0.30] rounded-xl mt-5 px-2 float-right',
-              actRowData.length === 0 ? '-mt-[250px]' : '',
+              actRowData.length === 0 ? '-mt-64' : '',
             )}
           >
             <div className="flex flex-row gap-4">
