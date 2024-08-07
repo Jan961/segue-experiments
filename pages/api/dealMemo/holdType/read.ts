e816d96f7b2dbc3538371d032ec996d4cd8b8bc7
Patrick Lafferty <seguesupport@robertckelly.co.uk>
2024-08-07 14:54:37 +0100

    PR comments actioned

--- pages/api/dealMemo/holdType/read.ts
+++ pages/api/dealMemo/holdType/read.ts
@@ -14,7 +14,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
       },
     });
 
-    res.json(holdTypes);
+    res.status(200).json(holdTypes);
   } catch (err) {
     console.log(err);
     res.status(403).json({ err: 'Error occurred while getting data for Deal Memo' });
