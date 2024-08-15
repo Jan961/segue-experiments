9544e3edf6837671edd7589907df39755e057e8e
David Kelly <davidrkelly07@gmail.com>
2024-08-15 11:22:37 +0100

    Defined runTag and gave it an empty string default so that if the booking for whatever reason doesnt have a run tag then it will collide with bookings on the same date anyway

--- components/bookings/modal/NewBooking/views/NewBookingDetailsView.tsx
+++ components/bookings/modal/NewBooking/views/NewBookingDetailsView.tsx
@@ -217,11 +217,12 @@ export default function NewBookingDetailsView({
         .getApi()
         .getDisplayedRowAtIndex(tableRef.current.getApi().getDisplayedRowCount() - 1);
       try {
+        const runTag = firstRow?.data?.runTag || '';
         const response = await axios.post('/api/bookings/conflict', {
           fromDate: firstRow.data.dateAsISOString,
           toDate: lastRow.data.dateAsISOString,
           productionId: production.Id,
-          runTag: firstRow.data.runTag,
+          runTag,
         });
         if (!isNullOrEmpty(response.data)) {
           updateBookingConflicts(response.data);
