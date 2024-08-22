825649d96cbc298cd3612d58f7153786aee7ce83
Matthew Waters <matthewwaters100@gmail.com>
2024-08-22 11:45:54 +0100

    Data test id's added for Marketing tables edit and delete buttons

--- components/global/salesTable/renderers/IconRowRenderer.tsx
+++ components/global/salesTable/renderers/IconRowRenderer.tsx
@@ -3,6 +3,7 @@ import Icon from 'components/core-ui-lib/Icon';
 
 type IconType = {
   name: string;
+  table: string;
   color: string;
   onClick: () => void;
 };
@@ -16,7 +17,7 @@ const IconRowRenderer = (props: IconRowProps) => {
     <div className="w-full h-full flex items-center justify-center gap-2">
       {props.iconList.map((icon) => (
         <Icon
-          testId={'activities-table-' + icon.name + '-button'}
+          testId={icon.table + '-table-' + icon.name + '-button'}
           color="#fff"
           key={icon.name}
           fill={icon.color}
