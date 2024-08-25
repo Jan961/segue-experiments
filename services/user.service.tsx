5c2c9a9780de11c3fd7207eeb25cd19cfd5bd3d5
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-08-25 16:11:41 +0100

    Added prims agenerate to git ignore

--- services/user.service.tsx
+++ services/user.service.tsx
@@ -43,7 +43,7 @@ function logout() {
   sessionStorage.removeItem('accountId');
 
   userSubject.next(null);
-  Router.push('/auth/sign-in');
+  Router.push('/sign-in');
 }
 
 function register(user) {
