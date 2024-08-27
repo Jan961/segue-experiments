ca87aa4ce39e05da84cff685bb0092ff53611d01
Matthew Waters <matthewwaters100@gmail.com>
2024-08-27 09:53:05 +0100

    Added Load Sales History Page, Production Selector Only Shows Archived Productions, Without the Style of Archived

---
+++ components/marketing/LoadSalesHistoryFilters.tsx
@@ -0,0 +1,11 @@
+import GlobalToolbar from 'components/toolbar';
+
+const LoadSalesHistoryFilters = () => {
+  return (
+    <div>
+      <GlobalToolbar title="" showArchivedCheck={false} onlyShowArchived={true} />
+    </div>
+  );
+};
+
+export default LoadSalesHistoryFilters;
