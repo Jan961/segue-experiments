b14a2fe24d7647879f3b23e529200887de409349
David Kelly <davidrkelly07@gmail.com>
2024-08-28 11:45:15 +0100

    Fixed the completion date not being nulled when progress is reverted from 100.

--- components/tasks/TasksTable.tsx
+++ components/tasks/TasksTable.tsx
@@ -11,6 +11,7 @@ import { ProductionTaskDTO } from 'interfaces';
 import { useRouter } from 'next/router';
 import AddTask from './modals/AddTask';
 import { isNullOrEmpty } from 'utils';
+import moment from 'moment';
 
 interface TasksTableProps {
   rowData?: any;
@@ -61,7 +62,12 @@ export default function TasksTable({
         ...task,
         Progress: progress,
         Notes: task.Notes,
-        ...(progress === 100 && { TaskCompletedDate: new Date().toISOString() }),
+        TaskCompletedDate:
+          progress < 100
+            ? null
+            : isNullOrEmpty(task.TaskCompletedDate)
+            ? moment.utc(new Date()).toString()
+            : task.TaskCompletedDate,
       };
       const updatedRowData = rowData.map((row) => {
         if (row.Id === task.Id) return updatedTask;
