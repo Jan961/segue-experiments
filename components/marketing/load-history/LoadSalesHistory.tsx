d9cdff7993bbaa8b8c7a9489b8e311cee5ba2dbf
Matthew Waters <matthewwaters100@gmail.com>
2024-08-29 12:46:10 +0100

    Can no longer Upload Files when no production selected, or spreadsheet already uploaded. Can now delete from table

--- components/marketing/load-history/LoadSalesHistory.tsx
+++ components/marketing/load-history/LoadSalesHistory.tsx
@@ -1,20 +1,25 @@
 import LoadSalesHistoryFilters from './LoadSalesHistoryFilters';
-import { Button, UploadModal, Table } from 'components/core-ui-lib';
+import { Button, UploadModal, Table, ConfirmationDialog } from 'components/core-ui-lib';
 import { loadSalesHistoryColDefs, styleProps } from '../table/tableConfig';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { uploadFile } from 'requests/upload';
 import { getFileUrl } from 'lib/s3';
 import { attachmentMimeTypes, UploadedFile } from 'components/core-ui-lib/UploadModal/interface';
 import { isNullOrEmpty } from 'utils';
 import SpreadsheetConfirmationModal from './SpreadsheetConfirmationModal';
 import { UploadParamType } from 'interfaces';
+import { useRecoilValue } from 'recoil';
+import { productionJumpState } from 'state/booking/productionJumpState';
 
 const LoadSalesHistory = () => {
   const [uploadModalVisible, setUploadModalVisible] = useState(false);
   const [confirmationModalVisible, setConfirmationModalVisible] = useState(false);
+  const [uploadDisabled, setUploadDisabled] = useState(true);
+  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
   const [salesHistoryRows, setSalesHistoryRows] = useState([]);
   const [uploadParams, setUploadParams] = useState<UploadParamType>(null);
   const [uploadedFile, setUploadedFile] = useState<UploadedFile[]>();
+  const { selected } = useRecoilValue(productionJumpState);
 
   const onSave = async (file, onProgress, onError, onUploadingImage) => {
     setConfirmationModalVisible(true);
@@ -46,6 +51,14 @@ const LoadSalesHistory = () => {
     }
   };
 
+  const deleteSalesHistory = () => {
+    // delete file from s3
+    // delete file from DB
+    // remove from table
+    setSalesHistoryRows([]);
+    setShowConfirmDelete(false);
+  };
+
   const handleCellClick = async (params) => {
     const column = params.column.colId;
     if (column === 'ViewBtn') {
@@ -54,8 +67,18 @@ const LoadSalesHistory = () => {
       } else {
         window.open(params.data.fileURL, '_blank');
       }
+    } else if (column === 'icons') {
+      setShowConfirmDelete(true);
     }
   };
+
+  useEffect(() => {
+    salesHistoryRows.length >= 1 ? setUploadDisabled(true) : setUploadDisabled(false);
+  }, [salesHistoryRows]);
+
+  useEffect(() => {
+    !selected ? setUploadDisabled(true) : setUploadDisabled(false);
+  }, []);
 
   return (
     <div>
@@ -63,7 +86,12 @@ const LoadSalesHistory = () => {
         <LoadSalesHistoryFilters />
         <div className="flex gap-x-3">
           <Button text="Download Template" className="w-[155px]" />
-          <Button text="Upload Template" className="w-[155px]" onClick={() => setUploadModalVisible(true)} />
+          <Button
+            text="Upload Template"
+            className="w-[155px]"
+            onClick={() => setUploadModalVisible(true)}
+            disabled={uploadDisabled}
+          />
         </div>
       </div>
       <Table
@@ -76,7 +104,7 @@ const LoadSalesHistory = () => {
         <UploadModal
           title="Upload Template"
           visible={uploadModalVisible}
-          info=""
+          info="Please upload Sales History information in the form of an Excel (.xlsx) spreadsheet. The format should take on the exact structure of the example spreadsheet provided."
           allowedFormats={attachmentMimeTypes.spreadsheetAttachment}
           onClose={() => {
             setUploadModalVisible(false);
@@ -93,6 +121,12 @@ const LoadSalesHistory = () => {
           uploadParams={uploadParams}
         />
       )}
+      <ConfirmationDialog
+        variant="delete"
+        show={showConfirmDelete}
+        onNoClick={() => setShowConfirmDelete(false)}
+        onYesClick={() => deleteSalesHistory()}
+      />
     </div>
   );
 };
