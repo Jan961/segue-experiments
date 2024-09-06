09cd2e33ce75edee31da8803ff8dd036e9b19450
Matthew Waters <matthewwaters100@gmail.com>
2024-09-06 09:05:09 +0100

    Added API just for deleting all date block events

---
+++ pages/api/marketing/load-history/delete-sales.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getDateBlockForProduction, deleteAllDateBlockEvents } from 'services/dateBlockService';
+
+interface RequestBody {
+  productionID: number;
+}
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { productionID }: RequestBody = req.body;
+
+    // Find DateBlock for ProductionID that isPrimary
+    const primaryDateBlock = await getDateBlockForProduction(productionID, true);
+    const primaryDateBlockID = primaryDateBlock[0].Id;
+
+    // Delete all associated Bookings/Rehearsals/GetInFitUp/Other events for that Primary Date Block - to be replaced.
+    await deleteAllDateBlockEvents(primaryDateBlockID);
+
+    res.status(200).json({ status: 'Success' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Error Creating Production File' });
+  }
+}
