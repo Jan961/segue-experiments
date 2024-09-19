66ae7b374b4c720bd9be352f97f8a2c6175cb448
David Kelly <davidrkelly07@gmail.com>
2024-09-19 14:30:51 +0100

    PR comments moving the service and changing some of the API because JS date object is horribly designed

--- services/getDateTypeFromId.ts
+++
@@ -1,4 +0,0 @@
-import prisma from 'lib/prisma';
-export const getDateTypeFromId = async (id: number) => {
-  return (await prisma.DateType.findFirst({ where: { Id: id }, select: { Name: true } }))?.Name;
-};
