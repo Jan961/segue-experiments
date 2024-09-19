b1880966638b80ae4d26350f3b3490273cb17306
David Kelly <davidrkelly07@gmail.com>
2024-09-19 09:45:03 +0100

    Added a comment explaining the reason for multiplying coords by 10000

--- utils/formatCoords.ts
+++ utils/formatCoords.ts
@@ -2,6 +2,7 @@ import { isNullOrEmpty } from './index';
 
 export const formatCoords = (coords) => {
   const { latitude, longitude } = coords;
+  //  The 10000 value is to make the coordinates to 4 decimal places for efficient storage as integers.
   return {
     latitude: !isNullOrEmpty(latitude) ? Math.round(latitude * 10000) : null,
     longitude: !isNullOrEmpty(longitude) ? Math.round(longitude * 10000) : null,
