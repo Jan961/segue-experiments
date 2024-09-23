fe098622dbe5e10be59fe5baff0b9cc45a770e9a
Matthew Waters <matthewwaters100@gmail.com>
2024-09-23 14:30:28 +0100

    email table fix

--- pages/api/email/templates/list.ts
+++ pages/api/email/templates/list.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from 'lib/prisma';
+import prisma from 'lib/prisma_master';
 import { EmailTemplate } from 'prisma/generated/prisma-master';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
