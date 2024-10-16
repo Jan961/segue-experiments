8b0ce751ff89b8c14e71737769ec694981ebd539
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-10-16 16:03:08 +0100

    cache prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,17 @@
 import { NextApiRequest } from 'next';
 import { PrismaClient } from 'prisma/generated/prisma-client';
 import { getOrganisationIdFromReq } from 'services/userService';
+import NodeCache from 'node-cache';
+
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 3600 });
+
+export function setCache(key, value) {
+  cache.set(key, value);
+}
+
+export function getCache(key) {
+  return cache.get(key);
+}
 
 const getPrismaClient = async (req: NextApiRequest): Promise<PrismaClient> => {
   if (req) {
@@ -12,7 +23,17 @@ const getPrismaClient = async (req: NextApiRequest): Promise<PrismaClient> => {
       }
 
       const prismaUrl = `${clientDBUrl}_${process.env.DEPLOYMENT_ENV}_Segue_${orgId}`;
+
+      // get prisma client from cache
+      const cachedClient = getCache(orgId);
+
+      if (cachedClient) {
+        return cachedClient as PrismaClient;
+      }
+
+      // create new prisma client and add it to cache
       const client = new PrismaClient({ datasourceUrl: prismaUrl });
+      setCache(orgId, client);
       return client;
     } catch (e) {
       console.log('Error getting prisma client', e);
