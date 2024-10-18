e37bfcd045a1a0586652f6325de066d39229241d
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-10-18 08:45:58 +0100

    Added tests for utility functions

---
+++ utils/specs/authUtils.spec.ts
@@ -0,0 +1,16 @@
+import { generateUserPassword, generateUserPin } from '../authUtils';
+
+describe('test generateUserPassword', () => {
+  it('should return a string that is 8 characters long, with capital letters, small letters, numbers and symbols', () => {
+    const password = generateUserPassword();
+    console.debug(password);
+    expect(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\W]{8,}$/.test(password)).toBe(true);
+  });
+});
+
+describe('test generateUserPin', () => {
+  it('should return a 5 digit number', () => {
+    const pin = generateUserPin();
+    expect(/^\d{5}$/.test(pin.toString())).toBe(true);
+  });
+});
