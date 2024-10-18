47db0f2b2b61cc0676a9f040476d0b2e7c710d80
Sachin Chandorkar <sachinchandorkar11@gmail.com>
2024-10-18 09:04:26 +0100

    Removed console log

--- utils/specs/authUtils.spec.ts
+++ utils/specs/authUtils.spec.ts
@@ -3,7 +3,6 @@ import { generateUserPassword, generateUserPin } from '../authUtils';
 describe('test generateUserPassword', () => {
   it('should return a string that is 8 characters long, with capital letters, small letters, numbers and symbols', () => {
     const password = generateUserPassword();
-    console.debug(password);
     expect(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\W]{8,}$/.test(password)).toBe(true);
   });
 });
