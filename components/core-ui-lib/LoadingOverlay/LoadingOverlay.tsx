dcec294cfdb853408272ea7e28e54c88dc52fc4f
Jan Wieczorek <jan@segue360.co.uk>
2024-11-06 12:35:03 +0000

    all changed

--- components/core-ui-lib/LoadingOverlay/LoadingOverlay.tsx
+++ components/core-ui-lib/LoadingOverlay/LoadingOverlay.tsx
@@ -1,18 +1,33 @@
 import Spinner from 'components/core-ui-lib/Spinner';
 import classNames from 'classnames';
+import { Loader } from '../index';
+import { variant } from '../Loader/Loader';
 
 interface LoaderProps {
   className?: string;
   testId?: string;
+  spinner?: boolean;
+  loaderClassName?: string;
+  loaderVariant?: variant;
 }
 
-export default function LoadingOverlay({ className, testId = 'loading-overlay' }: LoaderProps) {
+export default function LoadingOverlay({
+  className,
+  testId = 'loading-overlay',
+  spinner = true,
+  loaderClassName = 'ml-2',
+  loaderVariant = 'sm',
+}: LoaderProps) {
   return (
     <div
       data-testid={testId}
       className={classNames('inset-0 absolute bg-white bg-opacity-50 z-50 flex justify-center items-center', className)}
     >
-      <Spinner size="lg" />
+      {spinner ? (
+        <Spinner size="lg" />
+      ) : (
+        <Loader variant={loaderVariant} className={loaderClassName} iconProps={{ stroke: '#FFF' }} />
+      )}
     </div>
   );
 }
