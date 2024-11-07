c40fc82e45ef01cde9750bc90416a9f65680be70
Ryan Gardienr <Ryan@segue360.co.uk>
2024-11-07 14:40:24 +0000

    - Styling updates

--- components/core-ui-lib/Checkbox/Checkbox.tsx
+++ components/core-ui-lib/Checkbox/Checkbox.tsx
@@ -57,7 +57,20 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
           value={value}
           checked={checked}
           disabled={disabled}
-          className="peer relative appearance-none shrink-0 w-[1.1875rem] h-[1.1875rem] rounded-sm focus:ring-transparent  focus:checked:bg-primary-input-text checked:hover:bg-primary-input-text  checked:bg-primary-input-text border-2"
+          className={`
+          peer 
+          relative 
+          appearance-none 
+          shrink-0 
+          w-[1.1875rem] 
+          h-[1.1875rem] 
+          rounded-sm 
+          focus:ring-transparent  
+          focus:checked:bg-primary-input-text 
+          checked:hover:bg-primary-input-text  
+          checked:bg-primary-input-text
+          ${disabled ? 'bg-[#E9EBF0CC]' : 'bg-[#FFF]'}
+          `}
         />
 
         {label && (
