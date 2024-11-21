c8f7bab5581971a5fe3187b14f622669ecb1b4cd
Ryan Gardienr <Ryan@segue360.co.uk>
2024-11-21 14:00:08 +0000

    - Update

--- components/marketing/modal/ActivityModal.tsx
+++ components/marketing/modal/ActivityModal.tsx
@@ -76,7 +76,7 @@ export default function ActivityModal({
     } else if (variant === 'edit') {
       setActName(data.Name);
       setActType(data.ActivityTypeId);
-      setActDate(!data.Date ? null : startOfDay(safeDate(data.Date)));
+      setActDate(!safeDate(data.Date) ? null : startOfDay(safeDate(data.Date)));
       setActFollowUp(data.FollowUpRequired);
       setFollowUpDt(!data.DueByDate ? null : startOfDay(data.DueByDate));
       setCompanyCost(data.CompanyCost.toString());
