1456d851aea727200c0cc2d61a7ae89627896936
jan <jan@segue360.co.uk>
2024-12-04 16:07:13 +0000

    combine reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,15 @@ export default defineConfig({
       framework: 'next',
       bundler: 'webpack',
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      useInlineDiffs: true,
+      embeddedScreenshots: true,
+      reportDir: 'cypress/results',
+      reportFilename: '[name].html',
+      overwrite: true,
+      html: true,
+      json: true,
+    },
   },
 });
